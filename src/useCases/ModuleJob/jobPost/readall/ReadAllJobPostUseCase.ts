9637997d8aedbdbfec3fc7ede85352ce456f6ecc
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-10-17 19:36:27 -0700

    job post types update

--- src/useCases/ModuleJob/jobPost/readall/ReadAllJobPostUseCase.ts
+++ src/useCases/ModuleJob/jobPost/readall/ReadAllJobPostUseCase.ts
@@ -1,4 +1,5 @@
 import { IJobPost, JobPost } from "@entities/ModuleJob/JobPostModel";
+import { IPaginationResponse } from "@project-remote-job-board/shared/dist";
 import { JobPostRepository } from "@repositories/ModuleJob/jobPost/JobPostRepository";
 import { provide } from "inversify-binding-decorators";
 
@@ -6,7 +7,7 @@ import { provide } from "inversify-binding-decorators";
 export class ReadAllJobPostUseCase {
   constructor(private jobPostRepository: JobPostRepository) {}
 
-  public async readAll(filter: Record<string, unknown>): Promise<IJobPost[]> {
+  public async readAll(filter: Record<string, unknown>): Promise<IPaginationResponse<IJobPost>> {
     // return await this.jobPostRepository.readAll(JobPost, filter, false, null, true, 20);
 
     const results = await this.jobPostRepository.readAllPaginated(JobPost, filter, null, null, true);
