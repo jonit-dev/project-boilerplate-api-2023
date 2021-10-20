48b91ba646a7d6a8b35ce2c6126d364e89e9a965
gaiveknopf <avigaive@hotmail.com>
2021-10-20 08:01:11 -0300

    implements mongoose query parser helper

--- src/useCases/ModuleJob/jobPost/readall/ReadAllJobPostUseCase.ts
+++ src/useCases/ModuleJob/jobPost/readall/ReadAllJobPostUseCase.ts
@@ -1,5 +1,6 @@
 import { IJobPost, JobPost } from "@entities/ModuleJob/JobPostModel";
 import { IPaginationResponse } from "@project-remote-job-board/shared/dist";
+import { MongooseQueryParserHelper } from "@providers/adapters/MongooseQueryParserHelper";
 import { JobPostRepository } from "@repositories/ModuleJob/jobPost/JobPostRepository";
 import { provide } from "inversify-binding-decorators";
 
@@ -10,10 +11,12 @@ export class ReadAllJobPostUseCase {
   public async readAll(filter: Record<string, unknown>): Promise<IPaginationResponse<IJobPost>> {
     const offset = Number(filter.offset || 0);
     delete filter.offset;
+    const parser = new MongooseQueryParserHelper();
+    const parsedFilter = parser.queryParser(filter);
 
     const results = await this.jobPostRepository.readAllPaginated<IJobPost>(
       JobPost,
-      filter,
+      parsedFilter,
       null,
       null,
       true,
