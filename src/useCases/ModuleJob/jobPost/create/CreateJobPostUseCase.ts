4a1ee3b42e362c017cb5e5be857d731362303812
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-10-30 16:53:09 -0700

    delete jobPostData.company if not company is being passed to scrapper

--- src/useCases/ModuleJob/jobPost/create/CreateJobPostUseCase.ts
+++ src/useCases/ModuleJob/jobPost/create/CreateJobPostUseCase.ts
@@ -1,10 +1,10 @@
 import { Company } from "@entities/ModuleJob/CompanyModel";
-import { IJobPost, JobPost } from "@entities/ModuleJob/JobPostModel";
+import { JobPost } from "@entities/ModuleJob/JobPostModel";
+import { ICompany, IJobPost } from "@project-remote-job-board/shared/dist";
 import { NotFoundError } from "@providers/errors/NotFoundError";
 import { PlaceHelper } from "@providers/places/PlaceHelper";
 import { JobPostRepository } from "@repositories/ModuleJob/jobPost/JobPostRepository";
 import { provide } from "inversify-binding-decorators";
-
 import { CreateJobPostDTO } from "./CreateJobPostDTO";
 
 @provide(CreateJobPostUseCase)
@@ -27,15 +27,19 @@ export class CreateJobPostUseCase {
     let jobPostData = {
       ...createJobPostDTO,
       country,
-    };
+    } as unknown as IJobPost;
 
-    if (createJobPostDTO.company) {
+    const companyData = createJobPostDTO.company as ICompany;
+
+    if (companyData?.name) {
       const company = await Company.create(createJobPostDTO.company);
       await company.save();
       jobPostData = {
         ...jobPostData,
         company: company._id,
       };
+    } else {
+      delete jobPostData.company; // if we don't have a required name for the company, just delete it.
     }
 
     return await this.jobPostRepository.create(JobPost, jobPostData, null, null, null);
