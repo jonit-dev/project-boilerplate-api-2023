999be816549929a42e569211ea2c4fbec2bd3f2c
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-01-06 09:19:35 -0800

    db download dump

--- environment/db-download.sh
+++ environment/db-download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tput setaf 3;  echo "⚙️ Downloading database file"
+tput setaf 3;  echo "⚙️ Downloading database file. Please wait..."; tput sgr0
 tput setaf 2;
 
 [ ! -d "environment/backups" ] && mkdir environment/backups
 
-wget -O environment/backups/db-dump.zip "DROPBOX_LINK_HERE"
+wget -O environment/backups/db-dump.zip "https://www.dropbox.com/s/zqbbevdhwi7gbbe/db-dump.zip?dl=1"
