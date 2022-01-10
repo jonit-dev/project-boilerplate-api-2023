6bca652d2845320d7c3581f1edf8f611f1adfbf9
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-01-09 17:05:54 -0800

    mongo startup fix

--- docker_scripts/db_setup.sh
+++ docker_scripts/db_setup.sh
@@ -3,10 +3,5 @@
 echo '🪄 MongoDB: Creating application user and db 🪄'
 
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
-    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
-    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
-    var admin = db.getSiblingDB('admin');
-    admin.auth(rootUser, rootPassword);
-     
-    db.createUser({user: '${MONGO_INITDB_ROOT_USERNAME}', pwd: '${MONGO_INITDB_ROOT_PASSWORD}', roles:[{role:'dbOwner', db: '${MONGO_INITDB_DATABASE}'}]});
+   db.createUser({user: '${MONGO_INITDB_ROOT_USERNAME}', pwd: '${MONGO_INITDB_ROOT_PASSWORD}', roles:[{role:'dbOwner', db: '${MONGO_INITDB_DATABASE}'}]});
 EOF
