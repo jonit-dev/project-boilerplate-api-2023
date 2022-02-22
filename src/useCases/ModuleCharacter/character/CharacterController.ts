d914eab941efab4aa62ba376614657089e2268bc
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-02-21 18:08:33 -0800

    Character: Read

--- src/useCases/ModuleCharacter/character/CharacterController.ts
+++ src/useCases/ModuleCharacter/character/CharacterController.ts
@@ -2,7 +2,7 @@ import { ICharacter } from "@entities/ModuleSystem/CharacterModel";
 import { AuthMiddleware } from "@providers/middlewares/AuthMiddleware";
 import { DTOValidatorMiddleware } from "@providers/middlewares/DTOValidatorMiddleware";
 import { IAuthenticatedRequest } from "@providers/types/ExpressTypes";
-import { controller, httpPost, interfaces, request, requestBody } from "inversify-express-utils";
+import { controller, httpGet, httpPost, interfaces, request, requestBody, requestParam } from "inversify-express-utils";
 import { CreateCharacterDTO } from "./create/CreateCharacterDTO";
 import { CreateCharacterUseCase } from "./create/CreateCharacterUseCase";
 
@@ -18,5 +18,10 @@ export class CharacterController implements interfaces.Controller {
     const ownerId = request.user.id;
 
     return await this.createCharacterUseCase.create(newCharacter, ownerId);
+  }
+
+  @httpGet("/:id")
+  private async readCharacter(@requestParam("id") id: string): Promise<ICharacter> {
+    return await this.createCharacterUseCase.read(id);
   }
 }
