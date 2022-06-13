805a5d2d724e6bf8081eeb65a0429d9a1e6debda
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-06-12 20:47:42 -0700

    wing helmet

---
+++ src/providers/item/data/blueprints/ItemWingHelmet.ts
@@ -0,0 +1,17 @@
+import { IItem } from "@entities/ModuleInventory/ItemModel";
+import { ItemSubType, ItemType } from "@rpg-engine/shared";
+
+export const itemWingHelmet: Partial<IItem> = {
+  key: "wing-helmet",
+  type: ItemType.Armor,
+  subType: ItemSubType.Helmet,
+  textureAtlas: "items",
+  texturePath: "helmets/wing-helmet.png",
+  textureKey: "wing-helmet",
+  name: "Wing Helmet",
+  description:
+    "You see a wing helmet. It's decorated with wings and ancient depictions of the god Hermes, Mercury and of Roma.",
+  defense: 5,
+  weight: 12,
+  allowedEquipSlotType: [ItemSubType.Helmet],
+};
--- src/providers/item/data/index.ts
+++ src/providers/item/data/index.ts
@@ -3,10 +3,12 @@ import { itemCharacterBody } from "./blueprints/ItemCharacterBody";
 import { itemGroundBlood } from "./blueprints/ItemGroundBlood";
 import { itemNPCBody } from "./blueprints/ItemNPCBody";
 import { itemShortSword } from "./blueprints/ItemShortSword";
+import { itemWingHelmet } from "./blueprints/ItemWingHelmet";
 
 export const itemsBlueprintIndex: IBlueprint = {
   "short-sword": itemShortSword,
   "character-body": itemCharacterBody,
   "ground-blood": itemGroundBlood,
   "npc-body": itemNPCBody,
+  "wing-helmet": itemWingHelmet,
 };
