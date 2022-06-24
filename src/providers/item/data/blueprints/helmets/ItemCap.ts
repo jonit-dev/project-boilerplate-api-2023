036bb32d0e55ecc966faf5cdd12c6b788fb7b179
Aribeir0 <andrebnu1@gmail.com>
2022-06-24 17:38:02 -0300

    Added item helmet cap.

---
+++ src/providers/item/data/blueprints/helmets/ItemCap.ts
@@ -0,0 +1,16 @@
+import { IItem } from "@entities/ModuleInventory/ItemModel";
+import { ItemSlotType, ItemSubType, ItemType } from "@rpg-engine/shared";
+
+export const itemWingHelmet: Partial<IItem> = {
+  key: "cap",
+  type: ItemType.Armor,
+  subType: ItemSubType.Helmet,
+  textureAtlas: "items",
+  texturePath: "helmets/cap.png",
+  textureKey: "cap",
+  name: "Cap",
+  description: "Simple cap.",
+  defense: 2,
+  weight: 0.3,
+  allowedEquipSlotType: [ItemSlotType.Head],
+};
