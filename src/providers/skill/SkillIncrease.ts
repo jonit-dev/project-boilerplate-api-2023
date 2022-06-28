2a5860cbdedfc5fed05e6e3e3e0fae9984a02d51
Tom <tomasguerraalda@hotmail.com>
2022-06-28 10:51:57 -0300

    [RPG-287] Add skill increase logic when hit target and block hit

---
+++ src/providers/skill/SkillIncrease.ts
@@ -0,0 +1,96 @@
+import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
+import { Equipment } from "@entities/ModuleCharacter/EquipmentModel";
+import { Skill, ISkill } from "@entities/ModuleCharacter/SkillsModel";
+import { Item, IItem } from "@entities/ModuleInventory/ItemModel";
+import { ItemType, ItemSubType } from "@rpg-engine/shared/dist/types/item.types";
+import { ISkillDetails } from "@rpg-engine/shared/dist/types/skills.types";
+import { provide } from "inversify-binding-decorators";
+import { SkillCalculator } from "./SkillCalculator";
+
+// QUESTION: what about other weapons - ex: Dagger, Club (there's club skill but not weapon)
+const ItemSkill = new Map<ItemSubType | string, string>([
+  ["unarmed", "first"],
+  [ItemSubType.Sword, "sword"],
+  [ItemSubType.Dagger, "sword"],
+  [ItemSubType.Axe, "axe"],
+  [ItemSubType.Bow, "distance"],
+  [ItemSubType.Spear, "distance"],
+  [ItemSubType.Shield, "shielding"],
+]);
+
+@provide(SkillIncrease)
+export class SkillIncrease {
+  constructor(private skillCalculator: SkillCalculator) {}
+
+  public async increaseWeaponSP(character: ICharacter): Promise<void> {
+    // Get character skills
+    const skills = (await Skill.findById(character.skills)) as ISkill;
+    if (!skills) {
+      throw new Error(`skills not found for character ${character.id}`);
+    }
+
+    const equipment = await Equipment.findById(character.equipment);
+    if (!equipment) {
+      throw new Error(`equipment not found for character ${character.id}`);
+    }
+
+    // Get right and left hand items
+    // What if has weapons on both hands? for now, only one weapon per character is allowed
+    const rightHandItem = equipment.rightHand ? await Item.findById(equipment.rightHand) : undefined;
+    const leftHandItem = equipment.leftHand ? await Item.findById(equipment.leftHand) : undefined;
+
+    // ItemSubType Shield is of type Weapon, so check that the weapon is not subType Shield (because cannot attack with Shield)
+    let foundWeapon = false;
+    if (rightHandItem?.type === ItemType.Weapon && rightHandItem?.subType !== ItemSubType.Shield) {
+      foundWeapon = true;
+      this.increaseItemSP(skills, rightHandItem);
+    }
+
+    if (leftHandItem?.type === ItemType.Weapon && leftHandItem?.subType !== ItemSubType.Shield) {
+      foundWeapon = true;
+      this.increaseItemSP(skills, leftHandItem);
+    }
+
+    // If user has no weapons (unarmed), then update 'first' skill
+    if (!foundWeapon) {
+      this.increaseItemSP(skills, { subType: "unarmed" } as IItem);
+    }
+
+    await skills.save();
+  }
+
+  public async increaseShieldingSP(character: ICharacter): Promise<void> {
+    const skills = (await Skill.findById(character.skills)) as ISkill;
+    if (!skills) {
+      throw new Error(`skills not found for character ${character.id}`);
+    }
+
+    this.increaseItemSP(skills, { subType: ItemSubType.Shield } as IItem);
+    await skills.save();
+  }
+
+  public increaseItemSP(skills: ISkill, item: IItem): void {
+    const skillToUpdate = ItemSkill.get(item.subType);
+
+    if (!skillToUpdate) {
+      throw new Error(`skill not found for item subtype ${item.subType}`);
+    }
+
+    const updatedSkillDetails = skills[skillToUpdate] as ISkillDetails;
+    updatedSkillDetails.skillPoints++;
+    updatedSkillDetails.skillPointsToNextLevel = this.skillCalculator.calculateSPToNextLevel(
+      updatedSkillDetails.skillPoints,
+      updatedSkillDetails.level + 1
+    );
+
+    if (updatedSkillDetails.skillPointsToNextLevel <= 0) {
+      updatedSkillDetails.level++;
+      updatedSkillDetails.skillPointsToNextLevel = this.skillCalculator.calculateSPToNextLevel(
+        updatedSkillDetails.skillPoints,
+        updatedSkillDetails.level + 1
+      );
+    }
+
+    skills[skillToUpdate] = updatedSkillDetails;
+  }
+}
---
+++ src/providers/skill/__tests__/SkillIncrease.spec.ts
@@ -0,0 +1,170 @@
+import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
+import { IItem } from "@entities/ModuleInventory/ItemModel";
+import { ISkill, Skill } from "@entities/ModuleCharacter/SkillsModel";
+import { container, unitTestHelper } from "@providers/inversify/container";
+import { SkillIncrease } from "../SkillIncrease";
+import { calculateSPToNextLevel } from "../SkillCalculator";
+import { ItemSubType } from "@rpg-engine/shared";
+import { Error } from "mongoose";
+
+type TestCase = {
+  item: ItemSubType;
+  skill: string;
+};
+
+const simpleTestCases: TestCase[] = [
+  {
+    item: ItemSubType.Sword,
+    skill: "sword",
+  },
+  {
+    item: ItemSubType.Dagger,
+    skill: "sword",
+  },
+  {
+    item: ItemSubType.Axe,
+    skill: "axe",
+  },
+  {
+    item: ItemSubType.Bow,
+    skill: "distance",
+  },
+  {
+    item: ItemSubType.Spear,
+    skill: "distance",
+  },
+  {
+    item: ItemSubType.Shield,
+    skill: "shielding",
+  },
+];
+
+describe("SkillIncrease.spec.ts", () => {
+  let skillIncrease: SkillIncrease,
+    testCharacter: ICharacter,
+    initialSkills: ISkill,
+    initialLevel: number,
+    spToLvl2: number;
+
+  beforeAll(async () => {
+    await unitTestHelper.beforeAllJestHook();
+    skillIncrease = container.get<SkillIncrease>(SkillIncrease);
+    initialSkills = new Skill({
+      ownerType: "Character",
+    }) as ISkill;
+    initialLevel = initialSkills?.first.level;
+    spToLvl2 = calculateSPToNextLevel(initialSkills?.first.skillPoints, initialLevel + 1);
+    expect(spToLvl2).toBeGreaterThan(0);
+  });
+
+  beforeEach(async () => {
+    await unitTestHelper.beforeEachJestHook(true);
+
+    testCharacter = await unitTestHelper.createMockCharacter();
+    await testCharacter.populate("skills").execPopulate();
+  });
+
+  it("should throw error when passing not a weapon item", async () => {
+    const characterSkills = (await Skill.findById(testCharacter.skills)) as ISkill;
+    try {
+      skillIncrease.increaseItemSP(characterSkills, { subType: ItemSubType.Potion } as IItem);
+      throw new Error("this should have failed");
+    } catch (error: any | Error) {
+      expect(error.message).toBe(`skill not found for item subtype ${ItemSubType.Potion}`);
+    }
+  });
+
+  for (const test of simpleTestCases) {
+    it(`should increase '${test.skill}' skill points | Item: ${test.item}`, async () => {
+      const characterSkills = (await Skill.findById(testCharacter.skills)) as ISkill;
+
+      expect(characterSkills[test.skill].level).toBe(initialLevel);
+      expect(characterSkills[test.skill].skillPoints).toBe(0);
+
+      for (let i = 0; i < spToLvl2; i++) {
+        skillIncrease.increaseItemSP(characterSkills, { subType: test.item } as IItem);
+      }
+
+      expect(characterSkills[test.skill].level).toBe(initialLevel + 1);
+      expect(characterSkills[test.skill].skillPoints).toBe(spToLvl2);
+
+      expect(characterSkills[test.skill].skillPointsToNextLevel).toBe(
+        calculateSPToNextLevel(spToLvl2, initialLevel + 2)
+      );
+    });
+  }
+
+  it("should increase character's 'first' skill points (SP for next level - 1)", async () => {
+    for (let i = 1; i < spToLvl2; i++) {
+      await skillIncrease.increaseWeaponSP(testCharacter);
+    }
+
+    const updatedSkills = await Skill.findById(testCharacter.skills);
+
+    expect(updatedSkills?.first.level).toBe(initialLevel);
+    expect(updatedSkills?.first.skillPoints).toBe(spToLvl2 - 1);
+    expect(updatedSkills?.first.skillPointsToNextLevel).toBe(1);
+  });
+
+  it("should increase character's 'first' skill level and skill points (exact SP for next level)", async () => {
+    // 10 Points are needed to increase skill level to level 1
+    for (let i = 0; i < spToLvl2; i++) {
+      await skillIncrease.increaseWeaponSP(testCharacter);
+    }
+
+    const updatedSkills = await Skill.findById(testCharacter.skills);
+
+    expect(updatedSkills?.first.level).toBe(initialLevel + 1);
+    expect(updatedSkills?.first.skillPoints).toBe(spToLvl2);
+    expect(updatedSkills?.first.skillPointsToNextLevel).toBe(calculateSPToNextLevel(spToLvl2, initialLevel + 2));
+  });
+
+  it("should increase character's 'first' skill level and skill points", async () => {
+    const spToAdd = spToLvl2 + 5;
+    for (let i = 0; i < spToAdd; i++) {
+      await skillIncrease.increaseWeaponSP(testCharacter);
+    }
+
+    const updatedSkills = await Skill.findById(testCharacter.skills);
+
+    expect(updatedSkills?.first.level).toBe(initialLevel + 1);
+    expect(updatedSkills?.first.skillPoints).toBe(spToAdd);
+    expect(updatedSkills?.first.skillPointsToNextLevel).toBe(calculateSPToNextLevel(spToLvl2, initialLevel + 2) - 5);
+
+    // Check that other skills remained unchanged
+    expect(updatedSkills?.axe.level).toBe(initialSkills?.axe.level);
+    expect(updatedSkills?.axe.skillPoints).toBe(initialSkills?.axe.skillPoints);
+    expect(updatedSkills?.distance.level).toBe(initialSkills?.distance.level);
+    expect(updatedSkills?.distance.skillPoints).toBe(initialSkills?.distance.skillPoints);
+    expect(updatedSkills?.sword.level).toBe(initialSkills?.sword.level);
+    expect(updatedSkills?.sword.skillPoints).toBe(initialSkills?.sword.skillPoints);
+    expect(updatedSkills?.shielding.level).toBe(initialSkills?.shielding.level);
+    expect(updatedSkills?.shielding.skillPoints).toBe(initialSkills?.shielding.skillPoints);
+  });
+
+  it("should increase character's 'shielding' skill level and skill points", async () => {
+    for (let i = 0; i < spToLvl2; i++) {
+      await skillIncrease.increaseShieldingSP(testCharacter);
+    }
+
+    const updatedSkills = await Skill.findById(testCharacter.skills);
+
+    expect(updatedSkills?.shielding.level).toBe(initialLevel + 1);
+    expect(updatedSkills?.shielding.skillPoints).toBe(spToLvl2);
+    expect(updatedSkills?.shielding.skillPointsToNextLevel).toBe(calculateSPToNextLevel(spToLvl2, initialLevel + 2));
+
+    // Check that other skills remained unchanged
+    expect(updatedSkills?.axe.level).toBe(initialSkills?.axe.level);
+    expect(updatedSkills?.axe.skillPoints).toBe(initialSkills?.axe.skillPoints);
+    expect(updatedSkills?.distance.level).toBe(initialSkills?.distance.level);
+    expect(updatedSkills?.distance.skillPoints).toBe(initialSkills?.distance.skillPoints);
+    expect(updatedSkills?.sword.level).toBe(initialSkills?.sword.level);
+    expect(updatedSkills?.sword.skillPoints).toBe(initialSkills?.sword.skillPoints);
+    expect(updatedSkills?.first.level).toBe(initialSkills?.first.level);
+    expect(updatedSkills?.first.skillPoints).toBe(initialSkills?.first.skillPoints);
+  });
+
+  afterAll(async () => {
+    await unitTestHelper.afterAllJestHook();
+  });
+});
