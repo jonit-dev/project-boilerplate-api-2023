4e7a81e8230d7a40aa42daca14b691339cc64f88
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-07-04 22:43:41 -0700

    debug

--- jestInitialSetup.ts
+++ jestInitialSetup.ts
@@ -1,3 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import "express-async-errors";
 import "reflect-metadata";
+
+// increase the timeout for the test suite
+jest.setTimeout(30000);
