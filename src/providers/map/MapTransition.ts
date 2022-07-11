195ab2fccd29c363899188393ae6afa9576868b7
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-07-10 21:15:33 -0700

    scene transition

--- src/providers/map/MapTransition.ts
+++ src/providers/map/MapTransition.ts
@@ -1,9 +1,32 @@
+import { ITiledObject } from "@rpg-engine/shared";
 import { provide } from "inversify-binding-decorators";
-import { MapTiles } from "./MapTiles";
+import { MapLoader } from "./MapLoader";
+import { MapObjectsLoader } from "./MapObjectsLoader";
 
 @provide(MapTransition)
 export class MapTransition {
-  constructor(private mapTiles: MapTiles) {}
+  constructor(private mapObjectsLoader: MapObjectsLoader) {}
 
-  // public isTransitionAtXY(map: string, layer: MapLayers, gridX: number, gridY: number): boolean {}
+  public getTransitionAtXY(mapName: string, x: number, y: number): ITiledObject | undefined {
+    const map = MapLoader.maps.get(mapName);
+
+    if (!map) {
+      throw new Error(`MapTransition: Map "${mapName}" is not found!`);
+    }
+
+    const transitions = this.mapObjectsLoader.getObjectLayerData("Transitions", map);
+
+    if (!transitions) {
+      return;
+    }
+
+    const sameXTransitions = transitions.filter((transition) => transition.x === x);
+
+    if (sameXTransitions) {
+      const transition = sameXTransitions.find((transition) => {
+        return transition.y === y;
+      });
+      return transition;
+    }
+  }
 }
