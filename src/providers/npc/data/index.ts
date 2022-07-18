acf3c07acdb9712e21c85cc8f6bf8b5129201e9c
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-07-17 17:25:10 -0700

    knight and blue hair woman

--- src/providers/npc/data/index.ts
+++ src/providers/npc/data/index.ts
@@ -3,10 +3,12 @@ import { npcAgatha } from "./blueprints/NPCAgatha";
 import { npcAlice } from "./blueprints/NPCAlice";
 import { npcAnnie } from "./blueprints/NPCAnnie";
 import { npcFelicia } from "./blueprints/NPCFelicia";
+import { npcKnight1 } from "./blueprints/NPCKnight1";
 import { npcMaria } from "./blueprints/NPCMaria";
 import { npcOrc } from "./blueprints/NPCOrc";
 import { npcRat } from "./blueprints/NPCRat";
 import { npcSkeleton } from "./blueprints/NPCSkeleton";
+import { npcWomanBlueHair } from "./blueprints/NPCWomanBlueHair";
 
 export const npcsBlueprintIndex: IBlueprint = {
   agatha: npcAgatha,
@@ -17,4 +19,6 @@ export const npcsBlueprintIndex: IBlueprint = {
   orc: npcOrc,
   skeleton: npcSkeleton,
   rat: npcRat,
+  "knight-1": npcKnight1,
+  "woman-blue-hair": npcWomanBlueHair,
 };
