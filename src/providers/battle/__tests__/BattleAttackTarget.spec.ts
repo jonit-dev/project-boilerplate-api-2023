9a54f1a6066f95b61e2751917df97aa46b04152f
Tom <tomasguerraalda@hotmail.com>
2022-07-20 15:18:51 -0300

    [RPG-498] add tests for PVP battle in BattleAttackTarget.js

--- src/providers/battle/__tests__/BattleAttackTarget.spec.ts
+++ src/providers/battle/__tests__/BattleAttackTarget.spec.ts
@@ -116,4 +116,97 @@ describe("BattleAttackTarget.spec.ts", () => {
   });
 });
 
+describe("BattleAttackTarget.spec.ts | PVP battle", () => {
+  let battleAttackTarget: BattleAttackTarget;
+
+  let targetCharacter: ICharacter;
+  let attackerCharacter: ICharacter;
+
+  beforeAll(async () => {
+    await unitTestHelper.beforeAllJestHook();
+    battleAttackTarget = container.get<BattleAttackTarget>(BattleAttackTarget);
+  });
+
+  beforeEach(async () => {
+    await unitTestHelper.beforeEachJestHook(true);
+
+    targetCharacter = await unitTestHelper.createMockCharacter(null, {
+      hasEquipment: true,
+      hasSkills: true,
+    });
+    attackerCharacter = await unitTestHelper.createMockCharacter(null, {
+      hasEquipment: true,
+      hasSkills: true,
+    });
+    await targetCharacter.populate("skills").execPopulate();
+    await attackerCharacter.populate("skills").execPopulate();
+
+    attackerCharacter.x = FromGridX(0);
+    attackerCharacter.y = FromGridX(0);
+    await attackerCharacter.save();
+
+    targetCharacter.x = FromGridX(1);
+    targetCharacter.y = FromGridX(1);
+    await targetCharacter.save();
+  });
+
+  it("should NOT hit a target if attacker has melee attack type and target is out of range", async () => {
+    const hitTarget = jest.spyOn(battleAttackTarget, "hitTarget" as any);
+
+    const attacker = attackerCharacter;
+    attacker.x = FromGridX(0);
+    attacker.y = FromGridY(0);
+    await attacker.save();
+
+    const defender = targetCharacter;
+    defender.x = FromGridX(5);
+    defender.y = FromGridY(5);
+    await defender.save();
+
+    await battleAttackTarget.checkRangeAndAttack(attacker, defender);
+
+    // expect battleAttackTarget to not have been called
+
+    expect(hitTarget).not.toHaveBeenCalled();
+  });
+
+  it("should hit a target if attacker has melee attack type and target is in range", async () => {
+    // expect battleAttackTarget to have been called
+    const hitTarget = jest.spyOn(battleAttackTarget, "hitTarget" as any);
+
+    await battleAttackTarget.checkRangeAndAttack(targetCharacter, attackerCharacter);
+
+    expect(hitTarget).toHaveBeenCalled();
+  });
+
+  it("when battle event is a hit, it should decrease the target's health", async () => {
+    // @ts-ignore
+    jest.spyOn(battleAttackTarget.battleEvent, "calculateEvent" as any).mockImplementation(() => BattleEventType.Hit);
+    // @ts-ignore
+    jest.spyOn(battleAttackTarget.battleEvent, "calculateHitDamage" as any).mockImplementation(() => 50);
+
+    // @ts-ignore
+    const increaseSkillsOnBattle = jest.spyOn(battleAttackTarget.skillIncrease, "increaseSkillsOnBattle" as any);
+
+    // @ts-ignore
+    await battleAttackTarget.hitTarget(attackerCharacter, targetCharacter);
+
+    expect(targetCharacter.health).toBeLessThan(targetCharacter.maxHealth);
+    expect(increaseSkillsOnBattle).toHaveBeenCalled();
+  });
+
+  it("when battle event is a miss, it should not decrease the target's health", async () => {
+    // @ts-ignore
+    jest.spyOn(battleAttackTarget.battleEvent, "calculateEvent" as any).mockImplementation(() => BattleEventType.Miss);
+
+    // @ts-ignore
+    await battleAttackTarget.hitTarget(attackerCharacter, targetCharacter);
+
+    expect(targetCharacter.health).toBe(targetCharacter.maxHealth);
+  });
+
+  afterAll(async () => {
+    await unitTestHelper.afterAllJestHook();
+  });
+});
 // your code here
