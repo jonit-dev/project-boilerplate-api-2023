c2e5d1c5d508e01e87387d6b94d2a125a26c9b63
Daniel Michalichyn <daniel.henrique@st-one.io>
2022-07-22 15:24:11 -0300

    Creating chain and plated gloves blueprint

---
+++ src/providers/item/data/blueprints/gloves/ItemPlateGloves.ts
@@ -0,0 +1,17 @@
+import { IItem } from "@entities/ModuleInventory/ItemModel";
+import { ItemSlotType, ItemSubType, ItemType } from "@rpg-engine/shared";
+import { GlovesBlueprint } from "../../types/itemsBlueprintTypes";
+
+export const itemPlateGloves: Partial<IItem> = {
+  key: GlovesBlueprint.PlateGloves,
+  type: ItemType.Armor,
+  subType: ItemSubType.Glove,
+  textureAtlas: "items",
+  texturePath: "gloves/plate-gloves.png",
+  textureKey: "plate-gloves",
+  name: "Plate Gloves",
+  description: "A pair of plated gloves.",
+  defense: 10,
+  weight: 2,
+  allowedEquipSlotType: [ItemSlotType.LeftHand, ItemSlotType.RightHand],
+};
--- src/providers/item/data/blueprints/gloves/index.ts
+++ src/providers/item/data/blueprints/gloves/index.ts
@@ -1,8 +1,12 @@
 import { GlovesBlueprint } from "../../types/itemsBlueprintTypes";
+import { itemChainGloves } from "./ItemChainGloves";
 import { itemLeatherGloves } from "./ItemLeatherGloves";
+import { itemPlateGloves } from "./ItemPlateGloves";
 import { itemStuddedGloves } from "./ItemStuddedGloves";
 
 export const glovesBlueprintIndex = {
   [GlovesBlueprint.LeatherGloves]: itemLeatherGloves,
   [GlovesBlueprint.StuddedGloves]: itemStuddedGloves,
+  [GlovesBlueprint.ChainGloves]: itemChainGloves,
+  [GlovesBlueprint.PlateGloves]: itemPlateGloves,
 };
