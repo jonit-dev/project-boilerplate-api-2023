c1274821d723b18cf017c23c02da5d9ca4980340
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-07-22 17:14:54 -0700

    map permissions fix

--- src/providers/map/MapCompressionHelper.ts
+++ src/providers/map/MapCompressionHelper.ts
@@ -5,7 +5,9 @@ import JSZip from "jszip";
 export const createZipMap = async (mapName: string, mapObject: object, pathToSave: string): Promise<void> => {
   const data = compress(mapObject);
   const zip = new JSZip();
-  zip.file(`${mapName}.txt`, JSON.stringify(data));
+  zip.file(`${mapName}.txt`, JSON.stringify(data), {
+    unixPermissions: "755",
+  });
   const buffer = await zip.generateAsync({ type: "uint8array" });
   await fs.createWriteStream(`${pathToSave}/${mapName}.zip`).write(buffer);
 };
