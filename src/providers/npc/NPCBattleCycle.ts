f979c40a5e12cff9fd31f77f94551aa847e5777d
Daniel Michalichyn <daniel.henrique@st-one.io>
2022-08-30 11:58:02 -0300

    Removing console.log

--- src/providers/npc/NPCBattleCycle.ts
+++ src/providers/npc/NPCBattleCycle.ts
@@ -25,7 +25,6 @@ export class NPCBattleCycle {
   }
 
   public async clear(): Promise<void> {
-    console.log(`NPCBattleCycle clear called! ID:[${this.id}]`);
     clearInterval(this.interval);
     NPCBattleCycle.npcBattleCycles.delete(this.id);
 
