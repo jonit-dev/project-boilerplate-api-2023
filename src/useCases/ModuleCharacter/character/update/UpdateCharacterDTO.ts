e0cd2859eccc27dd707e442f4a36909b3f1cd2c6
Khobab <khobabc@gmail.com>
2022-09-08 23:06:10 -0700

    RPG-716, RPG-772 user can not update faction and race

--- src/useCases/ModuleCharacter/character/update/UpdateCharacterDTO.ts
+++ src/useCases/ModuleCharacter/character/update/UpdateCharacterDTO.ts
@@ -3,18 +3,10 @@ import { CharacterFactions } from "@rpg-engine/shared";
 import { IsOptional, IsString, IsEnum } from "class-validator";
 import { CreateCharacterDTO } from "../create/CreateCharacterDTO";
 
-export class UpdateCharacterDTO implements CreateCharacterDTO {
+export class UpdateCharacterDTO {
   @IsOptional()
   @IsString(tsDefaultDecorator("validation", "isType", { type: "string" }))
   name: string;
-
-  @IsOptional()
-  @IsEnum(CharacterFactions, tsEnumDecorator("validation", "isEnum", CharacterFactions))
-  faction: string;
-
-  @IsOptional()
-  @IsString(tsDefaultDecorator("validation", "isType", { type: "string" }))
-  race: string;
 
   @IsOptional()
   @IsString(tsDefaultDecorator("validation", "isType", { type: "string" }))
