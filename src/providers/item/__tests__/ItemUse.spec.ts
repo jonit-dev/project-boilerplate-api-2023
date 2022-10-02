56262fc389bb1398466d11103e1ba4ff0e5efd3a
Khobab <khobabc@gmail.com>
2022-10-02 00:41:14 -0700

    RPG-788 added tests for item usage

--- src/providers/item/__tests__/ItemUse.spec.ts
+++ src/providers/item/__tests__/ItemUse.spec.ts
@@ -2,13 +2,14 @@ import { container, unitTestHelper } from "@providers/inversify/container";
 import { ItemUse } from "../ItemUse";
 import { CharacterWeight } from "@providers/character/CharacterWeight";
 import { stackableItemMock } from "@providers/unitTests/mock/itemMock";
-import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
+import { Character, ICharacter } from "@entities/ModuleCharacter/CharacterModel";
 import { IItem, Item } from "@entities/ModuleInventory/ItemModel";
 import { IItemContainer, ItemContainer } from "@entities/ModuleInventory/ItemContainerModel";
-import { ItemSocketEvents } from "@rpg-engine/shared";
+import { CharacterSocketEvents, ItemSocketEvents } from "@rpg-engine/shared";
 import { SocketMessaging } from "@providers/sockets/SocketMessaging";
 import { itemApple } from "../data/blueprints/foods/ItemApple";
 import { EquipmentEquip } from "@providers/equipment/EquipmentEquip";
+import { itemLightLifePotion } from "../data/blueprints/potions/ItemLightLifePotion";
 
 describe("ItemUse.ts", () => {
   let itemUse: ItemUse;
@@ -172,4 +173,73 @@ describe("ItemUse.ts", () => {
       },
     });
   });
+
+  it("should increase character health incrementally for food item", async () => {
+    itemUsageMock.mockRestore();
+    jest.useFakeTimers({ advanceTimers: true, doNotFake: ["setInterval", "clearInterval"] });
+
+    (itemUse as any).applyItemUsage(itemApple, testCharacter.id);
+
+    await wait(0.2);
+
+    jest.advanceTimersByTime(11000);
+    await wait(0.2);
+
+    jest.advanceTimersByTime(11000);
+    await wait(0.2);
+
+    jest.advanceTimersByTime(11000);
+    await wait(0.2);
+
+    jest.advanceTimersByTime(11000);
+    await wait(0.2);
+
+    // extra call to make sure cycle is only running for 5 times
+    jest.advanceTimersByTime(11000);
+    await wait(0.2);
+
+    expect(sendEventToUser).toBeCalledTimes(5);
+
+    for (let i = 1; i <= 5; i++) {
+      expect(sendEventToUser).toHaveBeenNthCalledWith(i, testCharacter.channelId, CharacterSocketEvents.ItemConsumed, {
+        targetId: testCharacter._id,
+        health: testCharacter.health + i,
+      });
+    }
+
+    const character = (await Character.findById(testCharacter.id)) as unknown as ICharacter;
+    expect(character.health).toBe(testCharacter.health + 5);
+  });
+
+  it("should increase character health instantly for potions", async () => {
+    itemUsageMock.mockRestore();
+    jest.useFakeTimers({ advanceTimers: true, doNotFake: ["setInterval", "clearInterval"] });
+
+    (itemUse as any).applyItemUsage(itemLightLifePotion, testCharacter.id);
+
+    await wait(0.2);
+
+    // extra call to make sure cycle is only running for 5 times
+    jest.advanceTimersByTime(11000);
+    await wait(0.2);
+
+    expect(sendEventToUser).toBeCalledTimes(1);
+
+    expect(sendEventToUser).toHaveBeenLastCalledWith(testCharacter.channelId, CharacterSocketEvents.ItemConsumed, {
+      targetId: testCharacter._id,
+      health: testCharacter.health + 20,
+    });
+
+    const character = (await Character.findById(testCharacter.id)) as unknown as ICharacter;
+    expect(character.health).toBe(testCharacter.health + 20);
+  });
 });
+
+function wait(sec): Promise<void> {
+  return new Promise<void>((resolve) => {
+    const inter = setInterval(() => {
+      resolve();
+      clearInterval(inter);
+    }, sec * 1000);
+  });
+}
