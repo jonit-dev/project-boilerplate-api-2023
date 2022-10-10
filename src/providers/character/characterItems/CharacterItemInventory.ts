ca0a51a5a97233d9b5b4aa535098062c34e0061a
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-10-10 00:55:55 -0700

    EquipmentEquipInventory edge case

--- src/providers/character/characterItems/CharacterItemInventory.ts
+++ src/providers/character/characterItems/CharacterItemInventory.ts
@@ -1,6 +1,9 @@
 import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
+import { Equipment, IEquipment } from "@entities/ModuleCharacter/EquipmentModel";
 import { ItemContainer } from "@entities/ModuleInventory/ItemContainerModel";
 import { Item } from "@entities/ModuleInventory/ItemModel";
+import { itemsBlueprintIndex } from "@providers/item/data/index";
+import { ContainersBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 import { SocketMessaging } from "@providers/sockets/SocketMessaging";
 import { IItem } from "@rpg-engine/shared";
 import { provide } from "inversify-binding-decorators";
@@ -101,5 +104,30 @@ export class CharacterItemInventory {
     );
 
     return false;
+  }
+
+  public async addEquipmentToCharacter(character: ICharacter): Promise<void> {
+    const equipment = await this.createEquipmentWithInventory(character);
+
+    character.equipment = equipment._id;
+    await character.save();
+  }
+
+  public async createEquipmentWithInventory(character: ICharacter): Promise<IEquipment> {
+    const equipment = new Equipment();
+    equipment.owner = character._id;
+
+    const blueprintData = itemsBlueprintIndex[ContainersBlueprint.Backpack];
+
+    const backpack = new Item({
+      ...blueprintData,
+      owner: equipment.owner,
+    });
+    await backpack.save();
+
+    equipment.inventory = backpack._id;
+    await equipment.save();
+
+    return equipment;
   }
 }
