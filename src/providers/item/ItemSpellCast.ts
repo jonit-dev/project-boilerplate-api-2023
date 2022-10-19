55ea6b1dcddb7e6ac866e6e26590216f21a5e1c5
Khobab <khobabc@gmail.com>
2022-10-19 01:15:35 -0700

    RPG-891 spell validation

--- src/providers/item/ItemSpellCast.ts
+++ src/providers/item/ItemSpellCast.ts
@@ -1,14 +1,49 @@
 import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
 import { provide } from "inversify-binding-decorators";
 import { itemsBlueprintIndex } from "@providers/item/data/index";
+import { SocketMessaging } from "@providers/sockets/SocketMessaging";
+import { CharacterValidation } from "@providers/character/CharacterValidation";
 
 @provide(ItemSpellCast)
 export class ItemSpellCast {
+  constructor(private socketMessaging: SocketMessaging, private characterValidation: CharacterValidation) {}
+
   public isSpellCasting(msg: string): boolean {
     return !!this.getSpell(msg);
   }
 
-  public castSpell(magicWords: string, character: ICharacter): void {}
+  public castSpell(magicWords: string, character: ICharacter): boolean {
+    if (!this.characterValidation.hasBasicValidation(character)) {
+      return false;
+    }
+
+    const spell = this.getSpell(magicWords);
+
+    if (!this.isSpellCastingValid(spell, character)) {
+      return false;
+    }
+
+    return false;
+  }
+
+  private isSpellCastingValid(spell, character: ICharacter): boolean {
+    if (!spell) {
+      this.socketMessaging.sendErrorMessageToCharacter(character, "Sorry, spell not found.");
+      return false;
+    }
+
+    if (!character.learnedSpells || character.learnedSpells.indexOf(spell.key) < 0) {
+      this.socketMessaging.sendErrorMessageToCharacter(character, "Sorry, you have not learned this spell.");
+      return false;
+    }
+
+    if (character.mana < spell.manaCost) {
+      this.socketMessaging.sendErrorMessageToCharacter(character, "Sorry, you do not have mana to cast this spell.");
+      return false;
+    }
+
+    return true;
+  }
 
   private getSpell(magicWords: string): any {
     for (const key in itemsBlueprintIndex) {
--- src/providers/item/__tests__/ItemSpellCast.spec.ts
+++ src/providers/item/__tests__/ItemSpellCast.spec.ts
@@ -2,7 +2,10 @@ import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
 import { AnimationEffect } from "@providers/animation/AnimationEffect";
 import { container, unitTestHelper } from "@providers/inversify/container";
 import { SocketMessaging } from "@providers/sockets/SocketMessaging";
+import { CharacterValidation } from "@providers/character/CharacterValidation";
 import { ItemSpellCast } from "../ItemSpellCast";
+import { AnimationEffectKeys, UISocketEvents } from "@rpg-engine/shared";
+import { itemSelfHealing } from "../data/blueprints/spells/ItemSelfHealing";
 
 describe("ItemSpellCast.ts", () => {
   let itemSpellCast: ItemSpellCast;
@@ -21,7 +24,7 @@ describe("ItemSpellCast.ts", () => {
 
     testCharacter = await (
       await unitTestHelper.createMockCharacter(
-        { health: 50 },
+        { health: 50, learnedSpells: [itemSelfHealing.key] },
         { hasEquipment: false, hasInventory: false, hasSkills: true }
       )
     )
@@ -51,6 +54,64 @@ describe("ItemSpellCast.ts", () => {
 
     it("should not be self healing spell casting", () => {
       expect(itemSpellCast.isSpellCasting("heal me now ")).toBeFalsy();
+    });
+  });
+
+  it("should fail with spell not found", () => {
+    expect(itemSpellCast.castSpell("heal me now ", testCharacter)).toBeFalsy();
+
+    expect(sendEventToUser).toBeCalledTimes(1);
+
+    expect(sendEventToUser).toHaveBeenLastCalledWith(testCharacter.channelId, UISocketEvents.ShowMessage, {
+      message: "Sorry, spell not found.",
+      type: "error",
+    });
+  });
+
+  it("should call character validation", () => {
+    const characterValidationMock = jest.spyOn(CharacterValidation.prototype, "hasBasicValidation");
+    characterValidationMock.mockReturnValue(false);
+
+    expect(itemSpellCast.castSpell("heal me now", testCharacter)).toBeFalsy();
+    expect(characterValidationMock).toHaveBeenLastCalledWith(testCharacter);
+
+    characterValidationMock.mockRestore();
+  });
+
+  it("should fail with spell not learned", async () => {
+    const runTest = (): void => {
+      sendEventToUser.mockReset();
+
+      expect(itemSpellCast.castSpell("heal me now", testCharacter)).toBeFalsy();
+
+      expect(sendEventToUser).toBeCalledTimes(1);
+
+      expect(sendEventToUser).toHaveBeenLastCalledWith(testCharacter.channelId, UISocketEvents.ShowMessage, {
+        message: "Sorry, you have not learned this spell.",
+        type: "error",
+      });
+    };
+
+    testCharacter.learnedSpells = undefined;
+    await testCharacter.save();
+    runTest();
+
+    testCharacter.learnedSpells = [];
+    await testCharacter.save();
+    runTest();
+  });
+
+  it("should fail with not enough mana", async () => {
+    testCharacter.mana = (itemSelfHealing.manaCost ?? 1) - 1;
+    await testCharacter.save();
+
+    expect(itemSpellCast.castSpell("heal me now", testCharacter)).toBeFalsy();
+
+    expect(sendEventToUser).toBeCalledTimes(1);
+
+    expect(sendEventToUser).toHaveBeenLastCalledWith(testCharacter.channelId, UISocketEvents.ShowMessage, {
+      message: "Sorry, you do not have mana to cast this spell.",
+      type: "error",
     });
   });
 });
