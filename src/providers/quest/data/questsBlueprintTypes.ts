7052421d497f0a6175510a8f388ad360b49db36c
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-10-23 20:33:13 -0700

    final fix

--- src/providers/quest/data/questsBlueprintTypes.ts
+++ src/providers/quest/data/questsBlueprintTypes.ts
@@ -1,6 +1,6 @@
 export enum QuestsBlueprint {
   Example = "example",
-  OrcFortress = "orc-fortress",
+  OrcFortress = "kill-orcs",
   KillRats = "kill-rats",
   InteractionTrader = "interaction-trader",
   InteractionFather = "interaction-father",
