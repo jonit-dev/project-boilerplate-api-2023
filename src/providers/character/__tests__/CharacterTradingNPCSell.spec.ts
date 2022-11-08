d0202423ebd5f316b9a2a74d2e6190fbc599b659
Khobab <khobabc@gmail.com>
2022-11-07 19:07:20 -0800

    DRPG-886 added more test cases for sell trading

--- src/providers/character/__tests__/CharacterTradingNPCSell.spec.ts
+++ src/providers/character/__tests__/CharacterTradingNPCSell.spec.ts
@@ -2,7 +2,7 @@
 import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
 import { Equipment } from "@entities/ModuleCharacter/EquipmentModel";
 import { IItemContainer, ItemContainer } from "@entities/ModuleInventory/ItemContainerModel";
-import { IItem } from "@entities/ModuleInventory/ItemModel";
+import { IItem, Item } from "@entities/ModuleInventory/ItemModel";
 import { INPC, NPC } from "@entities/ModuleNPC/NPCModel";
 import { container, unitTestHelper } from "@providers/inversify/container";
 import { itemsBlueprintIndex } from "@providers/item/data/index";
@@ -268,6 +268,115 @@ describe("CharacterTradingNPCSell.ts", () => {
     expect(sendEventToUser).not.toBeCalled();
   });
 
+  it("should create two gold items if more gold earned than max stack size", async () => {
+    const items = [
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Arrow, { stackQty: 100 }),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Arrow, { stackQty: 100 }),
+    ];
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, items);
+
+    const sellItems = [
+      {
+        key: RangedWeaponsBlueprint.Slingshot,
+        qty: 2,
+      },
+      {
+        key: RangedWeaponsBlueprint.Arrow,
+        qty: 200,
+      },
+    ];
+
+    await characterTradingNPCSell.sellItemsToNPC(testCharacter, testNPCTrader, sellItems);
+
+    const updatedContainer = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+    expect(updatedContainer.slots[0]).not.toBeNull();
+    expect(updatedContainer.slots[0].key).toBe(OthersBlueprint.GoldCoin);
+    expect(updatedContainer.slots[0].stackQty).toBe(100);
+
+    expect(updatedContainer.slots[1]).not.toBeNull();
+    expect(updatedContainer.slots[1].key).toBe(OthersBlueprint.GoldCoin);
+    expect(updatedContainer.slots[1].stackQty).toBe(20);
+
+    expect(updatedContainer.slots[2]).toBeNull();
+    expect(updatedContainer.slots[3]).toBeNull();
+  });
+
+  it("should add gold to existing stack if possible", async () => {
+    const items = [
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Arrow, { stackQty: 100 }),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Arrow, { stackQty: 100 }),
+      await unitTestHelper.createMockItemFromBlueprint(OthersBlueprint.GoldCoin, { stackQty: 10 }),
+    ];
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, items);
+
+    const sellItems = [
+      {
+        key: RangedWeaponsBlueprint.Slingshot,
+        qty: 2,
+      },
+      {
+        key: RangedWeaponsBlueprint.Arrow,
+        qty: 200,
+      },
+    ];
+
+    await characterTradingNPCSell.sellItemsToNPC(testCharacter, testNPCTrader, sellItems);
+
+    const updatedContainer = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+    expect(updatedContainer.slots[0]).not.toBeNull();
+    expect(updatedContainer.slots[0].key).toBe(OthersBlueprint.GoldCoin);
+    expect(updatedContainer.slots[0].stackQty).toBe(30);
+
+    expect(updatedContainer.slots[4]).not.toBeNull();
+    expect(updatedContainer.slots[4].key).toBe(OthersBlueprint.GoldCoin);
+    expect(updatedContainer.slots[4].stackQty).toBe(100);
+
+    expect(updatedContainer.slots[1]).toBeNull();
+    expect(updatedContainer.slots[2]).toBeNull();
+    expect(updatedContainer.slots[3]).toBeNull();
+  });
+
+  it("should drop gold on map if no slot available", async () => {
+    const items = [
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Arrow, { stackQty: 100 }),
+    ];
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 2, items);
+
+    const sellItems = [
+      {
+        key: RangedWeaponsBlueprint.Arrow,
+        qty: 50,
+      },
+    ];
+
+    await characterTradingNPCSell.sellItemsToNPC(testCharacter, testNPCTrader, sellItems);
+
+    const updatedContainer = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+    expect(updatedContainer.slots[0]).not.toBeNull();
+    expect(updatedContainer.slots[0].key).toBe(RangedWeaponsBlueprint.Slingshot);
+
+    expect(updatedContainer.slots[1]).not.toBeNull();
+    expect(updatedContainer.slots[1].key).toBe(RangedWeaponsBlueprint.Arrow);
+    expect(updatedContainer.slots[1].stackQty).toBe(50);
+
+    const droppedGold = await Item.findOne({ key: OthersBlueprint.GoldCoin });
+
+    expect(droppedGold?.x).toBe(testCharacter.x);
+    expect(droppedGold?.y).toBe(testCharacter.y);
+    expect(droppedGold?.scene).toBe(testCharacter.scene);
+  });
+
   it("should return items to be sold", async () => {
     await characterTradingNPCSell.initializeSell(testNPCTrader._id, testCharacter);
 
@@ -406,6 +515,68 @@ describe("CharacterTradingNPCSell.ts", () => {
       type: "sell",
       characterItems: [],
       characterAvailableGold: 0,
+    });
+  });
+
+  it("should not return an item if it does not have a blueprint", async () => {
+    const items = [
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot, {
+        key: "invalid-slingshot-key",
+      }),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot),
+    ];
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, items);
+
+    await characterTradingNPCSell.initializeSell(testNPCTrader._id, testCharacter);
+
+    expect(sendEventToUser).toBeCalled();
+    expect(sendErrorMessageToCharacter).not.toBeCalled();
+
+    const slingShot = itemsBlueprintIndex[RangedWeaponsBlueprint.Slingshot];
+    expect(sendEventToUser).toHaveBeenLastCalledWith(testCharacter.channelId, CharacterTradeSocketEvents.TradeInit, {
+      npcId: testNPCTrader._id,
+      type: "sell",
+      characterItems: [
+        {
+          key: slingShot.key,
+          price: slingShot.sellPrice,
+          name: slingShot.name,
+          texturePath: slingShot.texturePath,
+          qty: 1,
+        },
+      ],
+      characterAvailableGold: 0,
+    });
+  });
+
+  it("should not return an item if it does not have a sell price", async () => {
+    const items = [
+      await unitTestHelper.createMockItemFromBlueprint(OthersBlueprint.GoldCoin, { stackQty: 10 }),
+      await unitTestHelper.createMockItemFromBlueprint(RangedWeaponsBlueprint.Slingshot),
+    ];
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, items);
+
+    await characterTradingNPCSell.initializeSell(testNPCTrader._id, testCharacter);
+
+    expect(sendEventToUser).toBeCalled();
+    expect(sendErrorMessageToCharacter).not.toBeCalled();
+
+    const slingShot = itemsBlueprintIndex[RangedWeaponsBlueprint.Slingshot];
+    expect(sendEventToUser).toHaveBeenLastCalledWith(testCharacter.channelId, CharacterTradeSocketEvents.TradeInit, {
+      npcId: testNPCTrader._id,
+      type: "sell",
+      characterItems: [
+        {
+          key: slingShot.key,
+          price: slingShot.sellPrice,
+          name: slingShot.name,
+          texturePath: slingShot.texturePath,
+          qty: 1,
+        },
+      ],
+      characterAvailableGold: 10,
     });
   });
 });
