67420a5c139bba825dc1c5042d589cc984c8e375
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-11-17 22:20:52 -0800

    steak damage

--- src/providers/item/data/blueprints/foods/ItemRawBeefSteak.ts
+++ src/providers/item/data/blueprints/foods/ItemRawBeefSteak.ts
@@ -1,4 +1,6 @@
+import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
 import { IItem } from "@entities/ModuleInventory/ItemModel";
+import { EffectableAttribute, ItemUsableEffect } from "@providers/item/helper/ItemUsableEffect";
 import { ItemSubType, ItemType } from "@rpg-engine/shared";
 import { FoodsBlueprint } from "../../types/itemsBlueprintTypes";
 
@@ -9,9 +11,12 @@ export const itemRawBeefSteak: Partial<IItem> = {
   textureAtlas: "items",
   texturePath: "foods/raw-beef-steak.png",
   name: "Raw Beef Steak",
-  description: "A raw beef steak that can be used for cooking, but can't be consumed directly.",
+  description: "A raw beef steak that can be used for cooking, but shouldn't be consumed raw.",
   weight: 3,
   maxStackSize: 10,
   basePrice: 20,
   hasUseWith: true,
+  usableEffect: (character: ICharacter) => {
+    ItemUsableEffect.apply(character, EffectableAttribute.Health, -1);
+  },
 };
