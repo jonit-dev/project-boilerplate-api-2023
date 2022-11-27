31f8c0e4a8ccb59e2b4c1832cca00ea02a585c9f
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-11-27 14:26:49 -0800

    skill out of sync

--- src/providers/skill/SkillCalculator.ts
+++ src/providers/skill/SkillCalculator.ts
@@ -1,3 +1,4 @@
+import { calculateSPToNextLevel, calculateXPToNextLevel, getSPForLevel, getXPForLevel } from "@rpg-engine/shared";
 import { provide } from "inversify-binding-decorators";
 
 @provide(SkillCalculator)
@@ -18,29 +19,3 @@ export class SkillCalculator {
     return getXPForLevel(level);
   };
 }
-
-// functions exported separately because they need to be used in our skill schema, and the container is not accessible there.
-
-export const getSPForLevel = (level: number): number => {
-  const totalSPOnLevel = Math.pow(level, 3) * 5;
-  return totalSPOnLevel;
-};
-
-export const getXPForLevel = (level: number): number => {
-  const totalXPOnLevel = Math.pow(level, 3) * 3;
-  return totalXPOnLevel;
-};
-
-export const calculateXPToNextLevel = (currentXP: number, level: number): number => {
-  const xpToNextLevel = getXPForLevel(level);
-  const xpToNextLevelDiff = xpToNextLevel - currentXP;
-
-  return Math.round(xpToNextLevelDiff * 100) / 100;
-};
-
-export const calculateSPToNextLevel = (currentSP: number, level: number): number => {
-  const spToNextLevel = getSPForLevel(level);
-  const spToNextLevelDiff = spToNextLevel - currentSP;
-
-  return Math.round(spToNextLevelDiff * 100) / 100;
-};
