043e79a939b4c37a2e608461d6bfb11cede242ec
İslam Uyanık <islamuyanik@gmail.com>
2022-12-10 03:19:11 +0300

    QuestKillDeer (#252)
    * kill deer
    
    * deer
    
    * UseWithItem spec fix
    
    * update
    
    * update
    
    * description
    
    * blueprint
    
    * conflict
    
    * fix
    
    Co-authored-by: Joao Paulo Furtado <joaopaulofurtado@live.com>

--- src/providers/quest/data/questsBlueprintTypes.ts
+++ src/providers/quest/data/questsBlueprintTypes.ts
@@ -2,6 +2,7 @@ export enum QuestsBlueprint {
   Example = "example",
   OrcFortress = "kill-orcs",
   KillRats = "kill-rats",
+  KillDeers = "kill-deers",
   InteractionTrader = "interaction-trader",
   InteractionFather = "interaction-father",
   InteractionCraftSword = "interaction-craft-sword",
