9c5f57495c3d82072390725764b1a2ea94a06385
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-12-27 14:16:24 -0800

    adjusted instances

--- src/providers/sockets/SocketEventsBinder.ts
+++ src/providers/sockets/SocketEventsBinder.ts
@@ -1,15 +1,17 @@
 import { BattleNetwork } from "@providers/battle/network/BattleNetwork";
 import { CharacterNetwork } from "@providers/character/network/CharacterNetwork";
 import { ChatNetwork } from "@providers/chat/network/ChatNetwork";
+import { appEnv } from "@providers/config/env";
+import { DepotNetwork } from "@providers/depot/network/DepotNetwork";
 import { EquipmentNetwork } from "@providers/equipment/network/EquipmentNetwork";
 import { ItemNetwork } from "@providers/item/network/ItemNetwork";
 import { ItemContainerNetwork } from "@providers/itemContainer/network/ItemContainerNetwork";
 import { NPCNetwork } from "@providers/npc/network/NPCNetwork";
 import { QuestNetwork } from "@providers/quest/network/QuestNetwork";
 import { SkillNetwork } from "@providers/skill/network/SkillNetwork";
-import { DepotNetwork } from "@providers/depot/network/DepotNetwork";
-import { ViewNetwork } from "@providers/view/network/ViewNetwork";
 import { UseWithNetwork } from "@providers/useWith/network/UseWithNetwork";
+import { ViewNetwork } from "@providers/view/network/ViewNetwork";
+import { EnvType } from "@rpg-engine/shared";
 import { provide } from "inversify-binding-decorators";
 import { SocketChannel } from "./SocketsTypes";
 
@@ -31,17 +33,43 @@ export class SocketEventsBinder {
   ) {}
 
   public bindEvents(channel: SocketChannel): void {
-    this.characterNetwork.onAddEventListeners(channel);
-    this.npcNetwork.onAddEventListeners(channel);
-    this.battleNetwork.onAddEventListeners(channel);
-    this.chatNetwork.onAddEventListeners(channel);
-    this.itemNetwork.onAddEventListeners(channel);
-    this.viewNetwork.onAddEventListeners(channel);
-    this.itemContainerNetwork.onAddEventListeners(channel);
-    this.equipmentNetwork.onAddEventListeners(channel);
-    this.skillNetwork.onAddEventListeners(channel);
-    this.questNetwork.onAddEventListeners(channel);
-    this.useWithNetwork.onAddEventListeners(channel);
-    this.depotNetwork.onAddEventListeners(channel);
+    switch (appEnv.general.ENV) {
+      case EnvType.Development:
+        this.characterNetwork.onAddEventListeners(channel);
+        this.npcNetwork.onAddEventListeners(channel);
+        this.battleNetwork.onAddEventListeners(channel);
+        this.chatNetwork.onAddEventListeners(channel);
+        this.itemNetwork.onAddEventListeners(channel);
+        this.viewNetwork.onAddEventListeners(channel);
+        this.itemContainerNetwork.onAddEventListeners(channel);
+        this.equipmentNetwork.onAddEventListeners(channel);
+        this.skillNetwork.onAddEventListeners(channel);
+        this.questNetwork.onAddEventListeners(channel);
+        this.useWithNetwork.onAddEventListeners(channel);
+        this.depotNetwork.onAddEventListeners(channel);
+        break;
+      case EnvType.Production:
+        const pm2Instance = process.env.NODE_APP_INSTANCE;
+
+        if (pm2Instance === "0") {
+          this.characterNetwork.onAddEventListeners(channel);
+          this.npcNetwork.onAddEventListeners(channel);
+          this.battleNetwork.onAddEventListeners(channel);
+          this.chatNetwork.onAddEventListeners(channel);
+          this.itemNetwork.onAddEventListeners(channel);
+          this.viewNetwork.onAddEventListeners(channel);
+        }
+
+        if (pm2Instance === "1") {
+          this.itemContainerNetwork.onAddEventListeners(channel);
+          this.equipmentNetwork.onAddEventListeners(channel);
+          this.skillNetwork.onAddEventListeners(channel);
+          this.questNetwork.onAddEventListeners(channel);
+          this.useWithNetwork.onAddEventListeners(channel);
+          this.depotNetwork.onAddEventListeners(channel);
+        }
+
+        break;
+    }
   }
 }
