04e65d4354c200a701b5e47b96b4a9984314183e
Anuradha Fernando <anufdo@gmail.com>
2023-01-02 04:47:25 +0530

    Rpg 113 rpg rpg 1178 api write additional test cases for low coverage files draft 4 (#303)
    * Trigger Entity effect
    
    * Entry Effect complete
    
    * Draft with NPCExperience, NPCSpawn, MapHelper, partially MapTransition and ItemPickupFromContainer
    
    * NPCExperience, NPCSpawn, MapHelper, partially MapTransition and ItemPickupFromContainer
    
    * item pickup remove comments
    
    * Creating new test. draft 2
    
    * Edit test GetMapMetadataUseCase and MapTransition
    
    * Create tests ItemDrop, MapControlTime and NPCSpawn
    
    * test-cases-for-low-coverage-files-draft-4
    
    * NPCBattleCycle test and NPCCycle test update
    
    * fixed timed test issue
    
    Co-authored-by: Joao Paulo Furtado <joaopaulofurtado@live.com>

--- src/providers/map/__tests__/MapSolids.spec.ts
+++ src/providers/map/__tests__/MapSolids.spec.ts
@@ -2,9 +2,11 @@
 import { container, unitTestHelper } from "@providers/inversify/container";
 import { MapLayers } from "@rpg-engine/shared";
 import { MapSolids } from "../MapSolids";
+import { MapTiles } from "../MapTiles";
 
 describe("MapSolids.ts", () => {
   let mapSolids: MapSolids;
+  let mapTiles: MapTiles;
   const mapName = "unit-test-map";
 
   beforeAll(async () => {
@@ -12,6 +14,7 @@ describe("MapSolids.ts", () => {
 
     await unitTestHelper.initializeMapLoader();
     mapSolids = container.get<MapSolids>(MapSolids);
+    mapTiles = {} as MapTiles;
   });
 
   beforeEach(async () => {
@@ -28,6 +31,213 @@ describe("MapSolids.ts", () => {
     expect(bigRock).toBeTruthy();
     expect(treeTop).toBeFalsy();
     expect(noTile).toBeFalsy();
+  });
+
+  it("should return true if the tile at the given position on the given map and layer is solid when the isTileSolid method is called with the CHECK_SINGLE_LAYER strategy", () => {
+    const mapTilesMock = {
+      isSolid: jest.fn().mockReturnValue(true),
+    };
+
+    // Create a mock implementation of the MapHelper class that returns the highest map layer
+    const mapHelperMock = {
+      getHighestMapLayer: jest.fn().mockReturnValue(5),
+    };
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // @ts-ignore
+    mapSolids.mapHelper = mapHelperMock;
+
+    // Call the isTileSolid method with the CHECK_SINGLE_LAYER strategy
+    const result = mapSolids.isTileSolid("map1", 1, 2, MapLayers.Ground, "CHECK_SINGLE_LAYER");
+
+    // Assert that the result is true
+    expect(result).toBe(true);
+  });
+
+  it("should return true if any of the tiles on layers below the given layer at the given position on the given map are solid when the isTileSolid method is called with the CHECK_ALL_LAYERS_BELOW strategy", () => {
+    const mapTilesMock = {
+      isSolid: jest.fn(),
+    };
+    const mapHelperMock = {
+      getHighestMapLayer: jest.fn(() => 3),
+    };
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // @ts-ignore
+    mapSolids.mapHelper = mapHelperMock;
+
+    // Set up the mock functions to return different values depending on the layer being checked
+    mapTilesMock.isSolid.mockImplementation((_map: string, _gridX: number, _gridY: number, layer: MapLayers) => {
+      if (layer === MapLayers.Ground) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    // Test the isTileSolid method with the CHECK_ALL_LAYERS_BELOW strategy
+    const result = mapSolids.isTileSolid("map", 0, 0, MapLayers.OverCharacter, "CHECK_ALL_LAYERS_BELOW");
+
+    // Assert that the result is as expected
+    expect(result).toBe(true);
+  });
+
+  it("should return true if any of the tiles on any layer at the given position on the given map are solid when the isTileSolid method is called with the CHECK_ALL_LAYERS strategy", () => {
+    const mapTilesMock = {
+      isSolid: jest.fn(),
+    };
+
+    // Set up the mock function to return different values depending on the layer being checked
+    mapTilesMock.isSolid.mockImplementation((_map: string, _gridX: number, _gridY: number, layer: MapLayers) => {
+      if (layer === MapLayers.Ground || layer === MapLayers.OverCharacter) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // Test the isTileSolid method with the CHECK_ALL_LAYERS strategy
+    const result = mapSolids.isTileSolid("map", 0, 0, MapLayers.OverCharacter, "CHECK_ALL_LAYERS");
+
+    // Assert that the result is as expected
+    expect(result).toBe(true);
+  });
+
+  it("should return false if no tiles at the given position on the given map and layer are solid when the isTileSolid method is called", () => {
+    // Mock the mapTiles and mapHelper dependencies of the MapSolids class
+    const mapTilesMock = {
+      isSolid: jest.fn(),
+    };
+    // Set up the mock function to always return false
+    mapTilesMock.isSolid.mockReturnValue(false);
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // Test the isTileSolid method with the CHECK_SINGLE_LAYER strategy
+    const result = mapSolids.isTileSolid("map", 0, 0, MapLayers.Ground, "CHECK_SINGLE_LAYER");
+
+    // Assert that the result is as expected
+    expect(result).toBe(false);
+  });
+
+  it("should return true if the tile at the given position on the given map and layer is a passage when the isTilePassage method is called with the CHECK_SINGLE_LAYER strategy", () => {
+    // Mock the mapTiles and mapHelper dependencies of the MapSolids class
+    const mapTilesMock = {
+      isPassage: jest.fn(),
+    };
+
+    // Set up the mock function to return true for any position and layer
+    mapTilesMock.isPassage.mockReturnValue(true);
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // Test the isTilePassage method with the CHECK_SINGLE_LAYER strategy
+    const result = mapSolids.isTilePassage("map", 0, 0, MapLayers.Ground, "CHECK_SINGLE_LAYER");
+
+    // Assert that the result is as expected
+    expect(result).toBe(true);
+  });
+
+  it("should return true if any of the tiles on layers below the given layer at the given position on the given map are passages when the isTilePassage method is called with the CHECK_ALL_LAYERS_BELOW strategy", () => {
+    // Mock the mapTiles and mapHelper dependencies of the MapSolids class
+    const mapTilesMock = {
+      isPassage: jest.fn(),
+    };
+
+    // Set up the mock function to return different values depending on the layer being checked
+    mapTilesMock.isPassage.mockImplementation((_map: string, _gridX: number, _gridY: number, layer: MapLayers) => {
+      if (layer === MapLayers.Ground) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // Test the isTilePassage method with the CHECK_ALL_LAYERS_BELOW strategy
+    const result = mapSolids.isTilePassage("map", 0, 0, MapLayers.OverCharacter, "CHECK_ALL_LAYERS_BELOW");
+
+    // Assert that the result is as expected
+    expect(result).toBe(true);
+  });
+
+  it("It should return true if any of the tiles on layers above the given layer at the given position on the given map are passages when the isTilePassage method is called with the CHECK_ALL_LAYERS_ABOVE strategy.", () => {
+    // Mock the mapTiles and mapHelper dependencies of the MapSolids class
+    const mapTilesMock = {
+      isPassage: jest.fn(),
+    };
+
+    // Set up the mock function to return different values depending on the layer being checked
+    mapTilesMock.isPassage.mockImplementation((_map: string, _gridX: number, _gridY: number, layer: MapLayers) => {
+      if (layer === MapLayers.Ground) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // Test the isTilePassage method with the CHECK_ALL_LAYERS_BELOW strategy
+    const result = mapSolids.isTilePassage("map", 0, 0, MapLayers.Ground, "CHECK_ALL_LAYERS_ABOVE");
+
+    // Assert that the result is as expected
+    expect(result).toBe(true);
+  });
+
+  it("should return true if any of the tiles on any layer at the given position on the given map are passages when the isTilePassage method is called with the CHECK_ALL_LAYERS strategy", () => {
+    // Mock the mapTiles and mapHelper dependencies of the MapSolids class
+    const mapTilesMock = {
+      isPassage: jest.fn(),
+    };
+
+    // Set up the mock function to return different values depending on the layer being checked
+    mapTilesMock.isPassage.mockImplementation((_map: string, _gridX: number, _gridY: number, layer: MapLayers) => {
+      if (layer === MapLayers.Ground) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // Test the isTilePassage method with the CHECK_ALL_LAYERS strategy
+    const result = mapSolids.isTilePassage("map", 0, 0, MapLayers.OverCharacter, "CHECK_ALL_LAYERS");
+
+    // Assert that the result is as expected
+    expect(result).toBe(true);
+  });
+
+  it("should return false if no tiles at the given position on the given map and layer are passages when the isTilePassage method is called", () => {
+    // Mock the mapTiles and mapHelper dependencies of the MapSolids class
+    const mapTilesMock = {
+      isPassage: jest.fn(),
+    };
+
+    // Set up the mock function to always return false
+    mapTilesMock.isPassage.mockReturnValue(false);
+
+    // @ts-ignore
+    mapSolids.mapTiles = mapTilesMock;
+
+    // Test the isTilePassage method with the CHECK_SINGLE_LAYER strategy
+    const result = mapSolids.isTilePassage("map", 0, 0, MapLayers.Ground, "CHECK_SINGLE_LAYER");
+
+    // Assert that the result is as expected
+    expect(result).toBe(false);
   });
 
   afterAll(async () => {
---
+++ src/providers/npc/__tests__/NPCBattleCycle.spec.ts
@@ -0,0 +1,102 @@
+import { Character } from "@entities/ModuleCharacter/CharacterModel";
+import { unitTestHelper } from "@providers/inversify/container";
+import { NPCBattleCycle, NPC_BATTLE_CYCLES } from "../NPCBattleCycle";
+
+let npcBattleCycles: NPCBattleCycle;
+let id: string;
+let fn: Function;
+let intervalSpeed: number;
+describe("NPCBattleCycle.ts", () => {
+  beforeAll(async () => {
+    await unitTestHelper.beforeAllJestHook();
+    id = "62b792030c3f470048781135";
+    fn = jest.fn();
+    intervalSpeed = -1;
+    // Create an NPCBattleCycle instance
+    npcBattleCycles = new NPCBattleCycle(id, fn, intervalSpeed);
+  });
+  beforeEach(async () => {
+    await unitTestHelper.beforeEachJestHook(true);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+  afterAll(async () => {
+    await unitTestHelper.afterAllJestHook();
+  });
+
+  it("should test that the NPCBattleCycle instance is correctly added to the NPC_BATTLE_CYCLES map when it is created", () => {
+    expect(NPC_BATTLE_CYCLES.has(id)).toBe(true);
+    expect(NPC_BATTLE_CYCLES.get(id)).toBe(npcBattleCycles);
+  });
+
+  it("should test that the function passed to the constructor is called at the specified interval", async () => {
+    // Create a mock function and track the number of times it is called
+
+    jest.useFakeTimers({
+      advanceTimers: true,
+    });
+
+    const mockFn = jest.fn();
+
+    // Create a new NPCBattleCycle instance with a 10ms interval and the mock function
+    const npcBattleCycle = new NPCBattleCycle(id, mockFn, 10);
+
+    // Wait 20ms to allow the function to be called twice
+    await new Promise((resolve) => setTimeout(resolve, 20));
+
+    // Stop the interval and remove the NPCBattleCycle instance from the map
+    await npcBattleCycle.clear();
+
+    // Assert that the mock function was called twice
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it("should test that the clearInterval function was called with the correct interval", async () => {
+    const clearIntervalSpy = jest.spyOn(global, "clearInterval");
+
+    await npcBattleCycles.clear();
+
+    // Verify that the clearInterval function was called with the correct interval
+    expect(clearIntervalSpy).toHaveBeenCalledWith(npcBattleCycles.interval);
+  });
+
+  it("should test that the clear method updates the target field of the Character document with the given id", async () => {
+    const updateOneSpy = jest.spyOn(Character, "updateOne");
+
+    await npcBattleCycles.clear();
+
+    // Verify that the updateOne function was called with the correct arguments
+    expect(updateOneSpy).toHaveBeenCalledWith({ _id: id }, { $unset: { target: 1 } });
+  });
+
+  it("should test that the clear method does not throw an error if the Character document with the given id does not exist", async () => {
+    // Mock the Character.updateOne method to return an empty object
+    // @ts-expect-error
+    jest.spyOn(Character, "updateOne").mockResolvedValue({});
+
+    // Call the clear method
+    await expect(npcBattleCycles.clear()).resolves.not.toThrowError();
+  });
+
+  it("should test that the clear method does not throw an error if the interval has already been cleared", async () => {
+    // // Mock the clearInterval function to return undefined
+    // jest.spyOn(global, "clearInterval").mockReturnValue(undefined);
+    // Clear the interval
+    await npcBattleCycles.clear();
+
+    // Call the clear method
+    await expect(npcBattleCycles.clear()).resolves.not.toThrowError();
+  });
+
+  it("should test that the clear method does not throw an error if the NPC_BATTLE_CYCLES map is empty", async () => {
+    NPC_BATTLE_CYCLES.clear();
+
+    // Clear the NPCBattleCycle instance
+    await npcBattleCycles.clear();
+
+    // Assert that the clear method did not throw an error
+    expect(() => {}).not.toThrowError();
+  });
+});
