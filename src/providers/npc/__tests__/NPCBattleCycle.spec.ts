835fe1c399cf48a31034eb5e5394dba7b1c9afd7
anuradha fernando <anufdo@gmail.com>
2023-01-01 17:27:59 +0530

    test-cases-for-low-coverage-files-draft-4

---
+++ src/providers/npc/__tests__/NPCBattleCycle.spec.ts
@@ -0,0 +1,81 @@
+import { Character } from "@entities/ModuleCharacter/CharacterModel";
+import { unitTestHelper } from "@providers/inversify/container";
+import { NPCBattleCycle } from "../NPCBattleCycle";
+
+let npcBattleCycles: NPCBattleCycle;
+let id: string;
+let fn: Function;
+let intervalSpeed: number;
+describe("NPCBattleCycle.ts", () => {
+  beforeAll(async () => {
+    await unitTestHelper.beforeAllJestHook();
+    id = "62b792030c3f470048781135";
+    fn = jest.fn();
+    intervalSpeed = -1;
+
+    // Create an NPCBattleCycle instance
+    npcBattleCycles = new NPCBattleCycle(id, fn, intervalSpeed);
+  });
+  beforeEach(async () => {
+    await unitTestHelper.beforeEachJestHook(true);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+  afterAll(async () => {
+    await unitTestHelper.afterAllJestHook();
+  });
+
+  it("should add the NPCBattleCycle instance to the npcBattleCycles map when it is created", (done) => {
+    jest.runAllTimers();
+    expect(NPCBattleCycle.npcBattleCycles.get(id)).toEqual(npcBattleCycles);
+    done();
+  });
+
+  it("should test that the clear method removes the NPCBattleCycle instance from the npcBattleCycles map and clears the interval", async () => {
+    const clearIntervalSpy = jest.spyOn(global, "clearInterval");
+
+    await npcBattleCycles.clear();
+
+    // Verify that the clearInterval function was called with the correct interval
+    expect(clearIntervalSpy).toHaveBeenCalledWith(npcBattleCycles.interval);
+
+    // Verify that the npcCycle instance was removed from the npcCycles map
+    expect(NPCBattleCycle.npcBattleCycles.has(id)).toBe(false);
+  });
+
+  it("should test that the clear method updates the target field of the Character document with the given id", async () => {
+    const updateOneSpy = jest.spyOn(Character, "updateOne");
+
+    await npcBattleCycles.clear();
+
+    // Verify that the updateOne function was called with the correct arguments
+    expect(updateOneSpy).toHaveBeenCalledWith({ _id: id }, { $unset: { target: 1 } });
+  });
+
+  it("should test that the clear method does not throw an error if the Character document with the given id does not exist", async () => {
+    // Mock the Character.updateOne method to return an empty object
+    // @ts-expect-error
+    jest.spyOn(Character, "updateOne").mockResolvedValue({});
+
+    // Call the clear method
+    await expect(npcBattleCycles.clear()).resolves.not.toThrowError();
+  });
+
+  it("should test that the clear method does not throw an error if the interval has already been cleared", async () => {
+    // Mock the clearInterval function to return undefined
+    jest.spyOn(global, "clearInterval").mockReturnValue(undefined);
+
+    // Call the clear method
+    await expect(npcBattleCycles.clear()).resolves.not.toThrowError();
+  });
+
+  it("should test that the clear method does not throw an error if the NPCBattleCycle instance is not found in the npcCycles map", async () => {
+    // Remove the NPCCycle instance from the npcCycles map
+    NPCBattleCycle.npcBattleCycles.delete(npcBattleCycles.id);
+
+    // Verify that the clear method does not throw an error
+    await expect(npcBattleCycles.clear()).resolves.not.toThrowError();
+  });
+});
--- src/providers/quest/__tests__/QuestSystem.spec.ts
+++ src/providers/quest/__tests__/QuestSystem.spec.ts
@@ -6,12 +6,18 @@ import { IItem } from "@entities/ModuleInventory/ItemModel";
 import { INPC } from "@entities/ModuleNPC/NPCModel";
 import { IQuest } from "@entities/ModuleQuest/QuestModel";
 import { QuestRecord } from "@entities/ModuleQuest/QuestRecordModel";
+import { CharacterItems } from "@providers/character/characterItems/CharacterItems";
+import { CharacterWeight } from "@providers/character/CharacterWeight";
+import { EquipmentSlots } from "@providers/equipment/EquipmentSlots";
 import { container, unitTestHelper } from "@providers/inversify/container";
+import { CraftingResourcesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
+import { ItemDrop } from "@providers/item/ItemDrop";
+import { MovementHelper } from "@providers/movement/MovementHelper";
 import { FriendlyNPCsBlueprint, HostileNPCsBlueprint } from "@providers/npc/data/types/npcsBlueprintTypes";
+import { SocketMessaging } from "@providers/sockets/SocketMessaging";
 import { InteractionQuestSubtype } from "@providers/unitTests/UnitTestHelper";
 import { QuestStatus, QuestType } from "@rpg-engine/shared";
 import { QuestSystem } from "../QuestSystem";
-import { CraftingResourcesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 
 describe("QuestSystem.ts", () => {
   let questSystem: QuestSystem,
@@ -46,7 +52,7 @@ describe("QuestSystem.ts", () => {
       subtype: InteractionQuestSubtype.craft,
     });
 
-    // asign quest to the testCharacter
+    // assign quest to the testCharacter
     await createQuestRecord(testKillQuest, testCharacter);
     await createQuestRecord(testInteractionQuest, testCharacter);
     await createQuestRecord(testInteractionCraftQuest, testCharacter);
@@ -74,7 +80,7 @@ describe("QuestSystem.ts", () => {
   it("should update quest and release rewards | type interaction", async () => {
     await questSystem.updateQuests(QuestType.Interaction, testCharacter, npcKey);
     expect(await testInteractionQuest.hasStatus(QuestStatus.Completed, testCharacter.id)).toEqual(true);
-    expect(releaseRewards).toBeCalledTimes(2);
+    expect(releaseRewards).toBeCalledTimes(1);
   });
 
   it("should update quest and release rewards | type interaction craft", async () => {
@@ -89,7 +95,205 @@ describe("QuestSystem.ts", () => {
 
     await questSystem.updateQuests(QuestType.Interaction, testCharacter, "");
     expect(await testInteractionCraftQuest.hasStatus(QuestStatus.Completed, testCharacter.id)).toEqual(true);
-    expect(releaseRewards).toBeCalledTimes(3);
+    expect(releaseRewards).toBeCalledTimes(1);
+  });
+
+  it("should test that the updateQuests method does not update the quest record or release rewards if objectivesData is empty", async () => {
+    // Create mock functions for the methods called by updateQuests
+    const getObjectivesDataMock = jest.fn(() => ({} as any));
+    const updateKillObjectiveMock = jest.fn();
+    const updateInteractionObjectiveMock = jest.fn();
+    const releaseRewardsMock = jest.fn();
+
+    // @ts-ignore
+    questSystem.getObjectivesData = getObjectivesDataMock;
+    // @ts-ignore
+    questSystem.updateKillObjective = updateKillObjectiveMock;
+    // @ts-ignore
+    questSystem.updateInteractionObjective = updateInteractionObjectiveMock;
+    // @ts-ignore
+    questSystem.releaseRewards = releaseRewardsMock;
+
+    await questSystem.updateQuests(QuestType.Kill, testCharacter, "some target key");
+
+    // Assert that the mock functions were called as expected
+    expect(getObjectivesDataMock).toHaveBeenCalledWith(testCharacter, QuestType.Kill);
+    expect(updateKillObjectiveMock).not.toHaveBeenCalled();
+    expect(updateInteractionObjectiveMock).not.toHaveBeenCalled();
+    expect(releaseRewardsMock).not.toHaveBeenCalled();
+  });
+
+  it("should test that the updateQuests method does not update the quest record or release rewards if the quest type is invalid", async () => {
+    // Mock the dependencies of the QuestSystem class
+    const socketMessagingMock = {
+      sendToSocket: jest.fn(),
+    } as unknown as SocketMessaging;
+
+    const characterWeightMock = {
+      addWeight: jest.fn(),
+    } as unknown as CharacterWeight;
+
+    const equipmentSlotsMock = {
+      addToSlot: jest.fn(),
+    } as unknown as EquipmentSlots;
+
+    const movementHelperMock = {
+      moveTo: jest.fn(),
+    } as unknown as MovementHelper;
+    const itemDropMock = {
+      drop: jest.fn(),
+    } as unknown as ItemDrop;
+    const characterItemsMock = {
+      removeItem: jest.fn(),
+    } as unknown as CharacterItems;
+
+    // @ts-ignore
+    questSystem.socketMessaging = socketMessagingMock;
+    // @ts-ignore
+    questSystem.characterWeight = characterWeightMock;
+    // @ts-ignore
+    questSystem.equipmentSlots = equipmentSlotsMock;
+    // @ts-ignore
+    questSystem.movementHelper = movementHelperMock;
+    // @ts-ignore
+    questSystem.itemDrop = itemDropMock;
+    // @ts-ignore
+    questSystem.characterItems = characterItemsMock;
+
+    const updateOneMock = jest.fn();
+    QuestRecord.updateOne = updateOneMock;
+
+    // Test the updateQuests method with an invalid quest type
+    await questSystem.updateQuests("invalid quest type" as any, {} as any, "targetKey");
+
+    // Assert that the quest record was not updated and rewards were not released
+    expect(QuestRecord.updateOne).not.toHaveBeenCalled();
+    // @ts-ignore
+    expect(socketMessagingMock.sendToSocket).not.toHaveBeenCalled();
+    // @ts-ignore
+    expect(characterWeightMock.addWeight).not.toHaveBeenCalled();
+    // @ts-ignore
+    expect(equipmentSlotsMock.addToSlot).not.toHaveBeenCalled();
+    // @ts-ignore
+    expect(movementHelperMock.moveTo).not.toHaveBeenCalled();
+    // @ts-ignore
+    expect(itemDropMock.drop).not.toHaveBeenCalled();
+    // @ts-ignore
+    expect(characterItemsMock.removeItem).not.toHaveBeenCalled();
+  });
+
+  it("should test that the updateQuests method does not update the quest record or release rewards if there are no quest records with the specified status", async () => {
+    // Mock the characterItems dependency of the QuestSystem class
+    const characterItemsMock = {
+      remove: jest.fn(),
+    };
+
+    // Test the updateQuests method with an empty objectivesData value
+    await questSystem.updateQuests(QuestType.Interaction, testCharacter, "targetKey");
+
+    // Assert that the updateOne method of the QuestRecord model was not called
+    expect(QuestRecord.updateOne).not.toHaveBeenCalled();
+
+    // Assert that the remove method of the characterItems mock was not called
+    expect(characterItemsMock.remove).not.toHaveBeenCalled();
+  });
+
+  it("should test that the updateQuests method does not update the quest record or release rewards if the target key does not match any of the objectives", async () => {
+    // Mock the dependencies of the QuestSystem class
+    const socketMessagingMock = {
+      sendMessage: jest.fn(),
+    };
+    const characterWeightMock = {};
+    const equipmentSlotsMock = {};
+    const movementHelperMock = {};
+    const itemDropMock = {};
+    const characterItemsMock = {};
+    // @ts-ignore
+    questSystem.socketMessaging = socketMessagingMock;
+    // @ts-ignore
+    questSystem.characterWeight = characterWeightMock;
+    // @ts-ignore
+    questSystem.equipmentSlots = equipmentSlotsMock;
+    // @ts-ignore
+    questSystem.movementHelper = movementHelperMock;
+    // @ts-ignore
+    questSystem.itemDrop = itemDropMock;
+    // @ts-ignore
+    questSystem.characterItems = characterItemsMock;
+
+    // Mock the getObjectivesData method to return a set of objectives and records
+    // @ts-ignore
+    questSystem.getObjectivesData = jest.fn(() => ({
+      objectives: [{ targetKey: "key1" }, { targetKey: "key2" }],
+      records: [{}],
+    }));
+
+    // Mock the updateInteractionObjective method to return undefined
+    // @ts-ignore
+    questSystem.updateInteractionObjective = jest.fn(() => undefined);
+
+    // Mock the QuestRecord.updateOne method to not be called
+    const updateOneSpy = jest.spyOn(QuestRecord, "updateOne");
+
+    // Test the updateQuests method with a target key that does not match any of the objectives
+    await questSystem.updateQuests(QuestType.Interaction, testCharacter, "key3");
+
+    // Assert that the QuestRecord.updateOne method was not called
+    expect(updateOneSpy).not.toHaveBeenCalled();
+  });
+
+  it("should throw an error in updateQuests if the quest type is invalid", async () => {
+    // Define a quest type that is not recognized by the QuestSystem
+    const invalidQuestType: QuestType = "invalid" as QuestType;
+
+    // Mock the getObjectivesData method to return a set of objectives and records
+    // @ts-ignore
+    questSystem.getObjectivesData = jest.fn(() => ({
+      objectives: [{ targetKey: "key1" }, { targetKey: "key2" }],
+      records: [{}],
+    }));
+
+    // Expect the updateQuests method to throw an error when called with the invalid quest type
+    await expect(questSystem.updateQuests(invalidQuestType, testCharacter, creatureKey)).rejects.toThrowError(
+      `Invalid quest type ${invalidQuestType}`
+    );
+  });
+
+  it("should return undefined with empty objectives", async () => {
+    const objectivesData = { objectives: [], records: [] };
+
+    // @ts-ignore
+    const result = await questSystem.updateKillObjective(objectivesData, "targetKey");
+
+    expect(result).toBe(undefined);
+  });
+
+  // it("should return an empty object if there are no quest records with the specified status", async () => {
+  //   // // Set up the mock functions to return the specified data
+  //   jest.spyOn(QuestRecord, "find").mockResolvedValue([]);
+  //   jest.spyOn(QuestObjectiveInteraction, "find").mockResolvedValue([]);
+  //   jest.spyOn(QuestObjectiveKill, "find").mockResolvedValue([]);
+
+  //   // Test the getObjectivesData method
+  //   // @ts-ignore
+  //   const result = await questSystem.getObjectivesData(testCharacter, QuestType.Interaction, QuestStatus.InProgress);
+
+  //   // Assert that the result is as expected
+  //   expect(result).toEqual({});
+  // });
+
+  // it("should throw an error in getObjectivesData if the quest type is invalid", async () => {
+  //   const invalidQuestType: QuestType = "invalid" as QuestType;
+
+  //   await expect(
+  //     // @ts-ignore
+  //     questSystem.getObjectivesData(testCharacter, invalidQuestType, QuestStatus.InProgress)
+  //   ).rejects.toThrowError(`invalid quest type: ${invalidQuestType}`);
+  // });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
 
   afterAll(async () => {
