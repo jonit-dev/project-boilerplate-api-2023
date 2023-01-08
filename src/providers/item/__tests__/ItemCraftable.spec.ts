bc2b62483d20b223c987ac81aff2e0d4bb008dd6
Khobab <khobabc@gmail.com>
2023-01-08 10:43:59 +0500

    Item Crafting (#318)
    * RPG-1285 added structure for craftable items book
    
    * RPG-1285 added functionality to load craftable items
    
    * RPG-1288 added crafting validation
    
    * RPG-1290 implemented item crafting and inventory refresh event
    
    * RPG-1290 fix for delete item by key and minor grammatical fixes

---
+++ src/providers/item/__tests__/ItemCraftable.spec.ts
@@ -0,0 +1,250 @@
+import { Character, ICharacter } from "@entities/ModuleCharacter/CharacterModel";
+import { IItemContainer, ItemContainer } from "@entities/ModuleInventory/ItemContainerModel";
+import { IItem } from "@entities/ModuleInventory/ItemModel";
+import { container, unitTestHelper } from "@providers/inversify/container";
+import { SocketMessaging } from "@providers/sockets/SocketMessaging";
+import { CharacterSocketEvents, ItemSocketEvents, ItemSubType, UISocketEvents } from "@rpg-engine/shared";
+import { ItemCraftable } from "../ItemCraftable";
+import { CraftingResourcesBlueprint } from "../data/types/itemsBlueprintTypes";
+import { itemManaPotion } from "../data/blueprints/potions/ItemManaPotion";
+import { itemCorruptionStaff } from "../data/blueprints/staffs/ItemCorruptionStaff";
+import { itemBlueFeather } from "../data/blueprints/crafting-resources/ItemBlueFeather";
+import { itemWaterBottle } from "../data/blueprints/crafting-resources/itemWaterBottle";
+import { itemHerb } from "../data/blueprints/crafting-resources/ItemHerb";
+import { CharacterValidation } from "@providers/character/CharacterValidation";
+import { recipeBolt } from "@providers/useWith/recipes/ranged-weapons/recipeBolt";
+import { MovementSpeed } from "@providers/constants/MovementConstants";
+
+describe("ItemCraftable.ts", () => {
+  let craftableItem: ItemCraftable;
+  let inventoryContainer: IItemContainer;
+  let testCharacter: ICharacter;
+  let inventory: IItem;
+  let sendEventToUser: jest.SpyInstance;
+  let items: IItem[];
+
+  beforeAll(async () => {
+    await unitTestHelper.beforeAllJestHook();
+
+    craftableItem = container.get<ItemCraftable>(ItemCraftable);
+  });
+
+  beforeEach(async () => {
+    await unitTestHelper.beforeEachJestHook(true);
+
+    testCharacter = await unitTestHelper.createMockCharacter(
+      { health: 50, mana: 50 },
+      { hasEquipment: true, hasInventory: true }
+    );
+
+    inventory = await testCharacter.inventory;
+    inventoryContainer = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+    items = [
+      await unitTestHelper.createMockItemFromBlueprint(CraftingResourcesBlueprint.Herb, { stackQty: 3 }),
+      await unitTestHelper.createMockItemFromBlueprint(CraftingResourcesBlueprint.BlueFeather, { stackQty: 1 }),
+      await unitTestHelper.createMockItemFromBlueprint(CraftingResourcesBlueprint.WaterBottle, { stackQty: 1 }),
+    ];
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, items);
+
+    sendEventToUser = jest.spyOn(SocketMessaging.prototype, "sendEventToUser");
+  });
+
+  it("should load craftable items", async () => {
+    await craftableItem.loadCraftableItems(ItemSubType.Magic, testCharacter);
+
+    expect(sendEventToUser).toHaveBeenCalledTimes(1);
+
+    const args = sendEventToUser.mock.calls[0];
+
+    expect(args[0]).toEqual(testCharacter.channelId);
+    expect(args[1]).toEqual(ItemSocketEvents.CraftableItems);
+
+    const craftableItems = expect.arrayContaining([
+      expect.objectContaining({
+        key: itemManaPotion.key,
+        canCraft: true,
+        name: itemManaPotion.name,
+        texturePath: itemManaPotion.texturePath,
+        ingredients: expect.arrayContaining([
+          {
+            key: itemHerb.key,
+            name: itemHerb.name,
+            qty: 3,
+            texturePath: itemHerb.texturePath,
+          },
+          expect.objectContaining({
+            key: itemBlueFeather.key,
+            qty: 1,
+          }),
+          expect.objectContaining({
+            key: itemWaterBottle.key,
+            qty: 1,
+          }),
+        ]),
+      }),
+      expect.objectContaining({
+        key: itemCorruptionStaff.key,
+        canCraft: false,
+      }),
+    ]);
+
+    expect(args[2]).toEqual(craftableItems);
+  });
+
+  it("sould craft non stackable item", async () => {
+    await craftableItem.craftItem(itemManaPotion.key!, testCharacter);
+
+    const container = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+    expect(container.slots[1]).toBe(null);
+    expect(container.slots[2]).toBe(null);
+
+    const potion = container.slots[0];
+    expect(potion).not.toBe(null);
+    expect(potion.key).toEqual(itemManaPotion.key);
+  });
+
+  it("sould craft stackable item", async () => {
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, [
+      await unitTestHelper.createMockItemFromBlueprint(CraftingResourcesBlueprint.SmallWoodenStick, { stackQty: 1 }),
+      await unitTestHelper.createMockItemFromBlueprint(CraftingResourcesBlueprint.SteelIngot, { stackQty: 1 }),
+    ]);
+
+    await craftableItem.craftItem(recipeBolt.outputKey!, testCharacter);
+
+    const container = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+    expect(container.slots[1]).toBe(null);
+
+    const bolt = container.slots[0];
+    expect(bolt).not.toBe(null);
+    expect(bolt.key).toEqual(recipeBolt.outputKey);
+    expect(bolt.stackQty).toBeGreaterThanOrEqual(recipeBolt.outputQtyRange[0]);
+    expect(bolt.stackQty).toBeLessThanOrEqual(recipeBolt.outputQtyRange[1]);
+  });
+
+  it("sould change character weight", async () => {
+    const originalWeight = testCharacter.weight;
+
+    await craftableItem.craftItem(itemManaPotion.key!, testCharacter);
+
+    const character = await Character.findById(testCharacter._id);
+    expect(character!.weight).not.toBe(originalWeight);
+  });
+
+  it("sould send inventory update event", async () => {
+    await craftableItem.craftItem(itemManaPotion.key!, testCharacter);
+
+    const container = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+    expect(sendEventToUser).toHaveBeenCalledTimes(2);
+
+    expect(sendEventToUser).toHaveBeenCalledWith(testCharacter.channelId!, CharacterSocketEvents.AttributeChanged, {
+      targetId: testCharacter._id,
+      speed: MovementSpeed.Slow,
+    });
+
+    expect(sendEventToUser).toHaveBeenCalledWith(
+      testCharacter.channelId!,
+      ItemSocketEvents.EquipmentAndInventoryUpdate,
+      {
+        inventory: container,
+        openEquipmentSetOnUpdate: false,
+        openInventoryOnUpdate: true,
+      }
+    );
+  });
+
+  it("should call character validation", async () => {
+    const performCraftingMock = jest.spyOn(ItemCraftable.prototype as any, "performCrafting");
+    performCraftingMock.mockImplementation();
+
+    const characterValidationMock = jest.spyOn(CharacterValidation.prototype, "hasBasicValidation");
+    characterValidationMock.mockReturnValue(false);
+
+    await craftableItem.craftItem(itemManaPotion.key!, testCharacter);
+    expect(performCraftingMock).not.toBeCalled();
+
+    expect(characterValidationMock).toHaveBeenLastCalledWith(testCharacter);
+
+    performCraftingMock.mockReset();
+    characterValidationMock.mockReset();
+    characterValidationMock.mockReturnValue(true);
+
+    await craftableItem.craftItem(itemManaPotion.key!, testCharacter);
+    expect(performCraftingMock).toBeCalled();
+  });
+
+  it("should not craft item if it does not have a blueprint", async () => {
+    const performCraftingMock = jest.spyOn(ItemCraftable.prototype as any, "performCrafting");
+    performCraftingMock.mockImplementation();
+
+    await craftableItem.craftItem("invalid-blueprint-key", testCharacter);
+    expect(performCraftingMock).not.toBeCalled();
+
+    expect(sendEventToUser).toHaveBeenCalledTimes(1);
+    expect(sendEventToUser).toHaveBeenCalledWith(testCharacter.channelId, UISocketEvents.ShowMessage, {
+      message: "Sorry, this item can not be crafted.",
+      type: "error",
+    });
+  });
+
+  it("should not craft item if it does not have a recipe", async () => {
+    const performCraftingMock = jest.spyOn(ItemCraftable.prototype as any, "performCrafting");
+    performCraftingMock.mockImplementation();
+
+    await craftableItem.craftItem(CraftingResourcesBlueprint.Wheat, testCharacter);
+    expect(performCraftingMock).not.toBeCalled();
+
+    expect(sendEventToUser).toHaveBeenCalledTimes(1);
+    expect(sendEventToUser).toHaveBeenCalledWith(testCharacter.channelId, UISocketEvents.ShowMessage, {
+      message: "Sorry, this item can not be crafted.",
+      type: "error",
+    });
+  });
+
+  it("should not craft item if character inventory does not have required items", async () => {
+    const performCraftingMock = jest.spyOn(ItemCraftable.prototype as any, "performCrafting");
+    performCraftingMock.mockImplementation();
+
+    const performTest = async (): Promise<void> => {
+      await craftableItem.craftItem(itemManaPotion.key!, testCharacter);
+      expect(performCraftingMock).not.toBeCalled();
+
+      expect(sendEventToUser).toHaveBeenCalledTimes(1);
+      expect(sendEventToUser).toHaveBeenCalledWith(testCharacter.channelId, UISocketEvents.ShowMessage, {
+        message: "Sorry, you do not have required items in your inventory.",
+        type: "error",
+      });
+
+      performCraftingMock.mockReset();
+      sendEventToUser.mockReset();
+    };
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, []);
+    await performTest();
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, [items[1]]);
+    await performTest();
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, [items[1], items[2]]);
+    await performTest();
+
+    await unitTestHelper.addItemsToInventoryContainer(inventoryContainer, 6, [
+      await unitTestHelper.createMockItemFromBlueprint(CraftingResourcesBlueprint.Herb, { stackQty: 2 }),
+      items[1],
+      items[2],
+    ]);
+    await performTest();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  afterAll(async () => {
+    await unitTestHelper.afterAllJestHook();
+  });
+});
---
+++ src/providers/item/network/ItemNetworkCraftable.ts
@@ -0,0 +1,30 @@
+import { SocketAuth } from "@providers/sockets/SocketAuth";
+import { SocketChannel } from "@providers/sockets/SocketsTypes";
+import { ICraftItemPayload, ILoadCraftBookPayload, ItemSocketEvents } from "@rpg-engine/shared";
+import { provide } from "inversify-binding-decorators";
+import { ItemCraftable } from "../ItemCraftable";
+
+@provide(ItemNetworkCraftable)
+export class ItemNetworkCraftable {
+  constructor(private socketAuth: SocketAuth, private itemCraftable: ItemCraftable) {}
+
+  public onCraftableItemsLoad(channel: SocketChannel): void {
+    this.socketAuth.authCharacterOn(
+      channel,
+      ItemSocketEvents.LoadCraftBook,
+      async (data: ILoadCraftBookPayload, character) => {
+        if (data) {
+          await this.itemCraftable.loadCraftableItems(data.itemSubType, character);
+        }
+      }
+    );
+  }
+
+  public onCraftItem(channel: SocketChannel): void {
+    this.socketAuth.authCharacterOn(channel, ItemSocketEvents.CraftItem, async (data: ICraftItemPayload, character) => {
+      if (data) {
+        await this.itemCraftable.craftItem(data.itemKey, character);
+      }
+    });
+  }
+}
