2266bc09ebe39aacc70a2e253ee6e5861d829867
Khobab <khobabc@gmail.com>
2023-01-15 06:56:33 +0500

    fixed quest description, quantity update of items on quest completion (#327)

--- src/providers/quest/data/blueprints/interaction/QuestInteractionIronArrow.ts
+++ src/providers/quest/data/blueprints/interaction/QuestInteractionIronArrow.ts
@@ -6,7 +6,7 @@ export const questInteractionCraftIronArrow = {
   key: QuestsBlueprint.InteractionsCraftIronArrow,
   title: "John's Offer",
   description:
-    "Greetings, adventurer! I am John, a seasoned trader who has traversed these lands for many years. I am in need of iron ore and wood sticks to craft a new batch of iron arrows for my hunting endeavors. If you can bring me 10 of each, I will reward you with a quiver of my finest iron arrows. Will you accept this quest and retrieve the materials for me?",
+    "Greetings, adventurer! I am John, a seasoned trader who has traversed these lands for many years. I am in need of iron ingot and wood sticks to craft a new batch of iron arrows for my hunting endeavors. If you can bring me 10 of each, I will reward you with a quiver of my finest iron arrows. Will you accept this quest and retrieve the materials for me?",
   rewards: [
     {
       itemKeys: [RangedWeaponsBlueprint.IronArrow],
@@ -16,11 +16,10 @@ export const questInteractionCraftIronArrow = {
   objectives: [
     {
       items: [
-        { itemKey: CraftingResourcesBlueprint.IronIngot, qty: 5 },
-        { itemKey: CraftingResourcesBlueprint.WoodenSticks, qty: 15 },
+        { itemKey: CraftingResourcesBlueprint.IronIngot, qty: 10 },
+        { itemKey: CraftingResourcesBlueprint.WoodenSticks, qty: 10 },
       ],
       type: QuestType.Interaction,
-      // Should we Accept qty? Like 10?
     },
   ],
 } as Partial<IQuest>;
