92b4552ed08947d4e63b966c77ea8fc620bcadd6
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-01-23 12:00:00 -0800

    hammer to trader joe

--- src/providers/npc/data/blueprints/friendly/NPCTrader.ts
+++ src/providers/npc/data/blueprints/friendly/NPCTrader.ts
@@ -45,6 +45,9 @@ export const npcTrader = {
       key: ToolsBlueprint.ButchersKnife,
     },
     {
+      key: ToolsBlueprint.Hammer,
+    },
+    {
       key: ToolsBlueprint.CarpentersAxe,
     },
     {
