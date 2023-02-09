7db6aa1f00064fba0b0ca79be6f3bf8e1944b9c0
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-02-08 19:14:42 -0800

    removed recipe golden armor

--- src/providers/useWith/recipes/armors/index.ts
+++ src/providers/useWith/recipes/armors/index.ts
@@ -1,13 +1,11 @@
 import { ArmorsBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 import { IUseWithCraftingRecipe } from "@providers/useWith/useWithTypes";
 import { recipeBronzeArmor } from "./recipeBronzeArmor";
-import { recipeGoldenArmor } from "./recipeGoldenArmor";
 import { recipeIronArmor } from "./recipeIronArmor";
 import { recipePlateArmor } from "./recipePlateArmor";
 
 export const recipeArmorsIndex: Record<string, IUseWithCraftingRecipe[]> = {
   [ArmorsBlueprint.IronArmor]: [recipeIronArmor],
-  [ArmorsBlueprint.GoldenArmor]: [recipeGoldenArmor],
   [ArmorsBlueprint.BronzeArmor]: [recipeBronzeArmor],
   [ArmorsBlueprint.PlateArmor]: [recipePlateArmor],
 };
