95286ffd13895b070c2beb5ebec85591dc0210fe
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-03-12 18:31:42 -0700

    more content fixes

--- src/providers/npc/data/blueprints/neutral/index.ts
+++ src/providers/npc/data/blueprints/neutral/index.ts
@@ -1,22 +1,16 @@
 import { NeutralNPCsBlueprint } from "@providers/npc/data/types/npcsBlueprintTypes";
-import { npcBlackWhiteSheep } from "./NPCBlackWhiteSheep";
-import { npcBrownSheep } from "./NPCBrownSheep";
+
 import { npcDeer } from "./NPCDeer";
 import { npcGiantBrownRabbit } from "./NPCGiantBrownRabbit";
 import { npcGiantGrayRabbit } from "./NPCGiantGrayRabbit";
 import { npcGiantWhiteRabbit } from "./NPCGiantWhiteRabbit";
-import { npcPig } from "./NPCPig";
+
 import { npcRedDeer } from "./NPCRedDeer";
-import { npcWhiteSheep } from "./NPCWhiteSheep";
 
 export const neutralNPCs = {
   [NeutralNPCsBlueprint.Deer]: npcDeer,
-  [NeutralNPCsBlueprint.Pig]: npcPig,
   [NeutralNPCsBlueprint.GiantBrownRabbit]: npcGiantBrownRabbit,
   [NeutralNPCsBlueprint.GiantWhiteRabbit]: npcGiantWhiteRabbit,
   [NeutralNPCsBlueprint.GiantGrayRabbit]: npcGiantGrayRabbit,
-  [NeutralNPCsBlueprint.BrownSheep]: npcBrownSheep,
-  [NeutralNPCsBlueprint.BlackWhiteSheep]: npcBlackWhiteSheep,
-  [NeutralNPCsBlueprint.WhiteSheep]: npcWhiteSheep,
   [NeutralNPCsBlueprint.RedDeer]: npcRedDeer,
 };
