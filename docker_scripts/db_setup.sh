a98f2c468dae07b8db44233b8d4eff951b725b74
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-03-13 22:29:32 -0700

    db setup fix

--- docker_scripts/db_setup.sh
+++ docker_scripts/db_setup.sh
@@ -2,6 +2,6 @@
 
 echo '🪄 MongoDB: Creating application user and db 🪄'
 
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
    db.createUser({user: '${MONGO_INITDB_ROOT_USERNAME}', pwd: '${MONGO_INITDB_ROOT_PASSWORD}', roles:[{role:'dbOwner', db: '${MONGO_INITDB_DATABASE}'}]});
 EOF
