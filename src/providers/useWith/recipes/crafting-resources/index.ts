4c3d6815d214e5a3205154fd65cff37197dc85a0
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-05-23 21:55:25 -0700

    bandage

--- src/providers/useWith/recipes/crafting-resources/index.ts
+++ src/providers/useWith/recipes/crafting-resources/index.ts
@@ -1,7 +1,9 @@
 import { CraftingResourcesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 import { IUseWithCraftingRecipe } from "@providers/useWith/useWithTypes";
+import { recipeBandage } from "./recipeBandage";
 import { recipeSteelIngot } from "./recipeSteelIngot";
 
 export const recipeCraftingResources: Record<string, IUseWithCraftingRecipe[]> = {
   [CraftingResourcesBlueprint.SteelIngot]: [recipeSteelIngot],
+  [CraftingResourcesBlueprint.Bandage]: [recipeBandage],
 };
