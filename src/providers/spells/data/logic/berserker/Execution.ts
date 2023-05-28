f74386be2780744de848592c9ecd0daf46e756d7
luiz-fischer <68759407+luiz-fischer@users.noreply.github.com>
2023-05-27 23:56:56 -0300

    fix: msg health > 30 (#613)

--- src/providers/spells/data/logic/berserker/Execution.ts
+++ src/providers/spells/data/logic/berserker/Execution.ts
@@ -2,37 +2,46 @@ import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
 import { INPC } from "@entities/ModuleNPC/NPCModel";
 import { CharacterDeath } from "@providers/character/CharacterDeath";
 import { NPCDeath } from "@providers/npc/NPCDeath";
+import { SocketMessaging } from "@providers/sockets/SocketMessaging";
 import { EntityType } from "@rpg-engine/shared";
 import { provide } from "inversify-binding-decorators";
 
 @provide(Execution)
 export class Execution {
-  constructor(private npcDeath: NPCDeath, private characterDeath: CharacterDeath) {}
+  constructor(
+    private npcDeath: NPCDeath,
+    private characterDeath: CharacterDeath,
+    private socketMessaging: SocketMessaging
+  ) {}
 
-  public async handleBerserkerExecution(character: ICharacter, target: ICharacter | INPC): Promise<void> {
-    const attacker = character;
-    const entityId = target._id;
-    const entityType = target.type as EntityType;
+  public async handleBerserkerExecution(attacker: ICharacter, target: ICharacter | INPC): Promise<void> {
+    const targetId = target._id;
+    const targetType = target.type as EntityType;
 
     try {
-      if (!attacker || !entityId || !entityType) {
+      if (!attacker || !targetId || !targetType) {
         throw new Error("Invalid parameters");
       }
 
-      if (attacker._id.toString() === entityId.toString()) {
+      if (attacker._id.toString() === targetId.toString()) {
         return;
       }
 
-      if (entityType !== EntityType.Character && entityType !== EntityType.NPC) {
+      if (targetType !== EntityType.Character && targetType !== EntityType.NPC) {
         throw new Error("Invalid entityType provided");
       }
 
       const healthPercent = Math.floor((100 * target.health) / target.maxHealth);
 
-      if (healthPercent <= 30) {
-        if (target.type === EntityType.Character) {
+      if (healthPercent > 30) {
+        this.socketMessaging.sendErrorMessageToCharacter(
+          attacker,
+          "The target's health is above 30%, you can't execute target."
+        );
+      } else {
+        if (targetType === EntityType.Character) {
           await this.characterDeath.handleCharacterDeath(attacker, target as ICharacter);
-        } else {
+        } else if (targetType === EntityType.NPC) {
           await this.npcDeath.handleNPCDeath(target as INPC);
         }
       }
