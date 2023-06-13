7a9be90ee15da01438000c1543a952c2f2ff0abb
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-06-13 08:12:26 -0700

    loot gold adjustments

--- src/providers/constants/LootConstants.ts
+++ src/providers/constants/LootConstants.ts
@@ -2,7 +2,7 @@
 export const NPC_LOOT_CHANCE_MULTIPLIER = 0.3;
 
 // Gold
-export const LOOT_GOLD_QTY_RATIO = 1; // when it drops, ratio that regulates how much gold it would be
+export const LOOT_GOLD_QTY_RATIO = 0.8; // when it drops, ratio that regulates how much gold it would be
 export const LOOT_GOLD_DROP_CHANCE = 0.6; // chance that a mob drops gold. Static chance. Do not suffer influence of NPC_LOOT_CHANCE_MULTIPLIER
 export const LOOT_GOLD_MAX_HEALTH_WEIGHT = 1;
 export const LOOT_GOLD_STRENGTH_WEIGHT = 1.5;
