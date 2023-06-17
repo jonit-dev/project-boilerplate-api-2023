fa34431e9af98b27bca6ae3466e1392a04cbf3cd
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-06-17 00:15:02 -0700

    time mock

--- src/providers/time/Time.ts
+++ src/providers/time/Time.ts
@@ -1,12 +1,21 @@
+import { appEnv } from "@providers/config/env";
 import { provide } from "inversify-binding-decorators";
 
 @provide(Time)
 export class Time {
   async waitForSeconds(seconds: number): Promise<void> {
+    if (appEnv.general.IS_UNIT_TEST) {
+      return;
+    }
+
     await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
   }
 
   async waitForMilliseconds(milliseconds: number): Promise<void> {
+    if (appEnv.general.IS_UNIT_TEST) {
+      return;
+    }
+
     await new Promise((resolve) => setTimeout(resolve, milliseconds));
   }
 }
