71a07291f90e385761b617fb78f14bbfcbde1c62
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-06-22 10:20:55 -0700

    locker refactoring and fix

--- src/providers/locks/Locker.ts
+++ src/providers/locks/Locker.ts
@@ -5,16 +5,21 @@ import { provide } from "inversify-binding-decorators";
 export class Locker {
   constructor(private inMemoryHashTable: InMemoryHashTable) {}
 
-  public async lock(key: string): Promise<void> {
-    await this.inMemoryHashTable.set("locks", key, true);
+  public async lock(key: string): Promise<boolean> {
+    const isLocked = await this.isLocked(key);
+    if (!isLocked) {
+      await this.inMemoryHashTable.set("locks", key, true);
+      return true;
+    }
+    return false;
   }
 
   public async unlock(key: string): Promise<void> {
-    await this.inMemoryHashTable.set("locks", key, false);
+    await this.inMemoryHashTable.delete("locks", key);
   }
 
   public async isLocked(key: string): Promise<boolean> {
-    const result = (await this.inMemoryHashTable.get("locks", key)) as unknown as boolean;
+    const result = !!(await this.inMemoryHashTable.get("locks", key));
 
     return result === true;
   }
