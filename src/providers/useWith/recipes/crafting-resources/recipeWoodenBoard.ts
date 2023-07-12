5d99cb4fc06343bcaa79dae25f66282a90842746
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-07-11 20:53:00 -0700

    wooden board

---
+++ src/providers/useWith/recipes/crafting-resources/recipeWoodenBoard.ts
@@ -0,0 +1,19 @@
+import { calculateMinimumLevel } from "@providers/crafting/CraftingMinLevelCalculator";
+import { CraftingResourcesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
+import { IUseWithCraftingRecipe } from "@providers/useWith/useWithTypes";
+import { CraftingSkill } from "@rpg-engine/shared";
+
+export const recipeWoodenBoard: IUseWithCraftingRecipe = {
+  outputKey: CraftingResourcesBlueprint.WoodenBoard,
+  outputQtyRange: [1, 3],
+  requiredItems: [
+    {
+      key: CraftingResourcesBlueprint.GreaterWoodenLog,
+      qty: 20,
+    },
+  ],
+  minCraftingRequirements: [
+    CraftingSkill.Lumberjacking,
+    calculateMinimumLevel([[CraftingResourcesBlueprint.GreaterWoodenLog, 20]]),
+  ],
+};
