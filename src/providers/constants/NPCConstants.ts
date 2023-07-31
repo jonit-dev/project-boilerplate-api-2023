d667095388add3f800d40add65512bb121f276fe
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-07-30 21:44:53 -0700

    npc nerfed

--- src/providers/constants/NPCConstants.ts
+++ src/providers/constants/NPCConstants.ts
@@ -1,9 +1,9 @@
 export const NPC_BASE_HEALTH_MULTIPLIER = 1.5;
 export const NPC_SPEED_MULTIPLIER = 0.75;
-export const NPC_SKILL_LEVEL_MULTIPLIER = 2;
-export const NPC_SKILL_STRENGTH_MULTIPLIER = 2;
-export const NPC_SKILL_DEXTERITY_MULTIPLIER = 2;
-export const NPC_SKILL_RESISTANCE_MULTIPLIER = 2;
+export const NPC_SKILL_LEVEL_MULTIPLIER = 1.5;
+export const NPC_SKILL_STRENGTH_MULTIPLIER = 1.5;
+export const NPC_SKILL_DEXTERITY_MULTIPLIER = 1.5;
+export const NPC_SKILL_RESISTANCE_MULTIPLIER = 1.5;
 
 // Performance
 export const NPC_MAX_SIMULTANEOUS_ACTIVE_PER_INSTANCE = 40; // remember that in pm2 (prod) this is multiplied by the number of instances (CPUs)
