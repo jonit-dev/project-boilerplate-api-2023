6c26cfdcf055fdb7352bd6dd9e67642bcdbacbcd
Quan Tran <downy1229@gmail.com>
2023-08-02 01:05:21 +0700

    RPG-2144 add spell exclusive to dwarf and elf

---
+++ src/providers/spells/data/blueprints/elf/index.ts
@@ -0,0 +1,6 @@
+import { SpellsBlueprint } from "@rpg-engine/shared";
+import { spellElvenArrow } from "./SpellElvenArrow";
+
+export const elfSpellsIndex = {
+  [SpellsBlueprint.ElvenArrow]: spellElvenArrow,
+};
