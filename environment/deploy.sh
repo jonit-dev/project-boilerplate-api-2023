20ca514ac2c69b67a44892999e3487ff35e1bef3
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-08-12 22:45:56 -0700

    test

---
+++ environment/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Additional deployment steps
+cd ~/definya/api
+
+# Db backup
+npm run db:export
+
+# Inform PM2 to initialize the graceful shutdown and run command
+echo "Shutting down instances..."
+docker exec rpg-api bash -c 'pm2 sendSignal SIGINT rpg-api'
+
+# Stop the container after shutting down all instances
+echo "Stopping container..."
+docker-compose stop rpg-api
+
+# Initialize container with new code
+echo "Restarting containers..."
+docker-compose restart
+
+echo "Deployment complete."
--- environment/post-receive.sh
+++ environment/post-receive.sh
@@ -19,23 +19,5 @@ done
 echo "Deploying project..."
 GIT_WORK_TREE=$DEPLOY_DIR git checkout -f
 
-# Additional deployment steps
-cd ~/definya/api
-
-# Db backup
-npm run db:export
-
-# Inform PM2 to initialize the graceful shutdown and run command
-echo "Shutting down instances..."
-docker exec rpg-api bash -c 'pm2 sendSignal SIGINT rpg-api'
-
-# Stop the container after shutting down all instances
-echo "Stopping container..."
-docker-compose stop rpg-api
-
-# Initialize container with new code
-echo "Restarting container..."
-docker-compose restart
-
-echo "Deployment complete."
+bash ./deploy.sh
 
