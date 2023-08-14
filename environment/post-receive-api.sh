d432f65d6b6e4caa9493fdf41e056f352506ebf8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-08-13 18:38:53 -0700

    deploy test

--- environment/post-receive-api.sh
+++ environment/post-receive-api.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# avoid npm not found error
+   export NVM_DIR="$HOME/.nvm"
+            [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 # Set the deployment directory
 DEPLOY_DIR="/home/jonit/definya/api"
 
