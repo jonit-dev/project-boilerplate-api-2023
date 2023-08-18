39d2d43fc8913dc7eeaf3d269a7c11ee7e194ca1
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-08-18 07:44:39 -0700

    refactoring, env fixes

--- src/providers/database/RedisManager.ts
+++ src/providers/database/RedisManager.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-async-promise-executor */
 /* eslint-disable no-void */
 import { appEnv } from "@providers/config/env";
 import { provideSingleton } from "@providers/inversify/provideSingleton";
@@ -13,29 +14,36 @@ export class RedisManager {
 
   constructor() {}
 
-  public async connect(): Promise<void> {
-    const redisConnectionUrl = `redis://${appEnv.database.REDIS_CONTAINER}:${appEnv.database.REDIS_PORT}`;
+  public connect(): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+      const redisConnectionUrl = `redis://${appEnv.database.REDIS_CONTAINER}:${appEnv.database.REDIS_PORT}`;
 
-    this.client = createClient({
-      url: redisConnectionUrl,
-    });
-    try {
-      await this.client.connect();
-
-      // @ts-ignore
-      void applySpeedGooseCacheLayer(mongoose, {
-        redisUri: redisConnectionUrl,
+      this.client = createClient({
+        url: redisConnectionUrl,
       });
 
-      if (!appEnv.general.IS_UNIT_TEST) {
-        console.log("✅ Redis Client Connected");
-      }
-    } catch (error) {
-      console.log("❌ Redis initialization error: ", error);
-    }
+      this.client.on("error", (err) => {
+        console.log("❌ Redis error:", err);
+        reject(err); // If you want connection failures to reject the Promise
+      });
 
-    this.client.on("error", (err) => {
-      console.log("❌ Redis error:", err);
+      try {
+        await this.client.connect();
+
+        // @ts-ignore
+        void applySpeedGooseCacheLayer(mongoose, {
+          redisUri: redisConnectionUrl,
+        });
+
+        if (!appEnv.general.IS_UNIT_TEST) {
+          console.log("✅ Redis Client Connected");
+        }
+
+        resolve();
+      } catch (error) {
+        console.log("❌ Redis initialization error: ", error);
+        reject(error); // Reject the Promise if there's an error during connection
+      }
     });
   }
 }
--- src/providers/server/ServerBootstrap.ts
+++ src/providers/server/ServerBootstrap.ts
@@ -49,12 +49,7 @@ export class ServerBootstrap {
       await this.execOneTimeOperations();
     } else {
       // Production/Staging with PM2
-
-      console.log("Current process id", process.env.pm_id);
-      console.log(this.pm2Helper.pickLastCPUInstance());
-
       if (process.env.pm_id === this.pm2Helper.pickLastCPUInstance()) {
-        console.log("Executing one time operations");
         await this.execOneTimeOperations();
       }
     }
