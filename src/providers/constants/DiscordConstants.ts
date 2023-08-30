a6a0a8638ef000d56fb591e3fd65c09e10949abe
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-08-30 07:23:00 -0700

    bot fixes

--- src/providers/constants/DiscordConstants.ts
+++ src/providers/constants/DiscordConstants.ts
@@ -1,9 +1,8 @@
 export const DISCORD_CHANNEL_IDS = {
-  "pvp-and-wars": "1063552162459820073",
+  "pvp-and-wars": "1146449423304499273",
   raids: "1146299331243147314",
   trade: "1080731826915643492",
   bans: "1087945359378100274",
-  "level-up": "1146302899253678141",
 };
 
 export type DiscordChannelName = keyof typeof DISCORD_CHANNEL_IDS;
