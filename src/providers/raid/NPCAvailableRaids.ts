003db68204687e2cedf06b8532bbe7cfdc2b13b5
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-08-31 09:55:35 -0700

    raid changes update

--- src/providers/raid/NPCAvailableRaids.ts
+++ src/providers/raid/NPCAvailableRaids.ts
@@ -5,21 +5,21 @@ export const availableRaids: IRaid[] = [
     name: "Ilya Orc's Invasion",
     key: "orc-raid-ilya",
     startingMessage: "Orcs are invading Ilya! Defend the city!", // optional
-    triggeringChance: 20,
-    minDuration: 5,
+    triggeringChance: 5,
+    minDuration: 15,
   },
   {
     name: "Goblin Gangs",
     key: "goblin-gang-ilya",
     startingMessage: "Beware! Goblin gangs are assaulting travelers.", // optional
-    triggeringChance: 30,
+    triggeringChance: 10,
     minDuration: 10,
   },
   {
     name: "Orc Fortress Reinforcements",
     key: "orc-fortress-reinforcements",
     startingMessage: "Orc fortress is receiving reinforcements! Be Careful!", // optional
-    triggeringChance: 10,
-    minDuration: 15,
+    triggeringChance: 15,
+    minDuration: 10,
   },
 ];
