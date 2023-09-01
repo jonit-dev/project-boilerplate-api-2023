dec0c0993cee04f7213d42023377743902bc1e65
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-08-31 21:53:00 -0700

    minor fix marketplace warn

--- src/providers/constants/DiscordConstants.ts
+++ src/providers/constants/DiscordConstants.ts
@@ -3,6 +3,7 @@ export const DISCORD_CHANNEL_IDS = {
   raids: "1146299331243147314",
   trade: "1080731826915643492",
   bans: "1087945359378100274",
+  marketplaceWarns: "1147031074308882453",
 };
 
 export type DiscordChannelName = keyof typeof DISCORD_CHANNEL_IDS;
