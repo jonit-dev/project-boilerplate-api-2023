d3c4a3f5f42d2b84dfdac9cb46ea72448df375d9
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-09-02 11:53:51 -0700

    update test

--- environment/post-receive-client.sh
+++ environment/post-receive-client.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # avoid npm not found error
    export NVM_DIR="$HOME/.nvm"
             [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -25,3 +24,23 @@ done
 # Deploy the project
 echo "Deploying project..."
 git --work-tree=$DEPLOY_DIR --git-dir=$REPO_DIR checkout -f $BRANCH
+
+
+echo "🐳Building docker image..."
+docker build -t definya/definya-team:api-latest .
+docker push definya/definya-team:api-latest
+
+# Update repository code
+cd ~/definya/client
+
+# Clean untracked files if any
+git clean -fd 
+
+# Checkout the release branch and pull the latest changes
+git checkout release
+git pull origin release
+
+# Update the service to restart containers and force image update
+echo "🐳Restarting swarm service..."
+
+docker service update --force swarm-stack_rpg-client
