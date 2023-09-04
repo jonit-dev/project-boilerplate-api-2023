ac6a4655ec02e832a43b7de73838fc506ca8c397
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-09-03 19:55:39 -0700

    game adjusments, decresed difficulty of elite mobs, reduced craft drop ratio, etc

--- src/providers/constants/LootConstants.ts
+++ src/providers/constants/LootConstants.ts
@@ -2,8 +2,8 @@
 export const NPC_LOOT_CHANCE_MULTIPLIER = 0.1;
 
 // Gold
-export const LOOT_GOLD_QTY_RATIO = 0.6; // when it drops, ratio that regulates how much gold it would be
-export const LOOT_GOLD_DROP_CHANCE = 0.6; // chance that a mob drops gold. Static chance. Do not suffer influence of NPC_LOOT_CHANCE_MULTIPLIER
+export const LOOT_GOLD_QTY_RATIO = 0.4; // when it drops, ratio that regulates how much gold it would be
+export const LOOT_GOLD_DROP_CHANCE = 0.4; // chance that a mob drops gold. Static chance. Do not suffer influence of NPC_LOOT_CHANCE_MULTIPLIER
 export const LOOT_GOLD_MAX_HEALTH_WEIGHT = 1;
 export const LOOT_GOLD_STRENGTH_WEIGHT = 1.5;
 export const LOOT_GOLD_RESISTANCE_WEIGHT = 1;
@@ -11,8 +11,8 @@ export const LOOT_GOLD_LEVEL_WEIGHT = 1.5;
 
 // Crafting Materials
 
-export const LOOT_CRAFTING_MATERIAL_DROP_CHANCE = 2; // chance that a mob drops crafting materials.
+export const LOOT_CRAFTING_MATERIAL_DROP_CHANCE = 1; // chance that a mob drops crafting materials.
 ("");
 // Food
 
-export const LOOT_FOOD_DROP_CHANCE = 0.3; // chance that a mob drops food. Static chance. Do not suffer influence of NPC_LOOT_CHANCE_MULTIPLIER
+export const LOOT_FOOD_DROP_CHANCE = 0.5; // chance that a mob drops food. Static chance. Do not suffer influence of NPC_LOOT_CHANCE_MULTIPLIER
