e1d5610362ab559e476ed37b4698e3c0d273515b
guiazca <guiazca@gmail.com>
2023-09-06 16:44:48 +0100

    Rpg 2644 create blueprint get ep (#813)
    * creating EP
    
    * adding blueprint EP
    
    * adding more code
    
    * final solution
    
    * fixing
    
    * adding getBlueprintKeys EP
    
    * removing AuthMiddleware
    
    * fixing EP
    
    * clean import
    
    * removing not used EP
    
    * mores fixes

--- src/useCases/ModuleSystem/blueprint/BlueprintController.ts
+++ src/useCases/ModuleSystem/blueprint/BlueprintController.ts
@@ -1,25 +1,13 @@
-import { BlueprintManager, BlueprintNamespaces } from "../../../providers/blueprint/BlueprintManager";
-import { AuthMiddleware } from "@providers/middlewares/AuthMiddleware";
-import { controller, httpGet, interfaces, request, requestParam } from "inversify-express-utils";
-import { IAuthenticatedRequest } from "@providers/types/ServerTypes";
-import { AvailableBlueprints } from "@providers/item/data/types/itemsBlueprintTypes";
+import { BlueprintNamespaces } from "../../../providers/blueprint/BlueprintManager";
+import { controller, httpGet, interfaces, requestParam } from "inversify-express-utils";
 import { BlueprintUseCase } from "./BlueprintUseCase";
 
 @controller("/blueprints")
 export class BlueprintController implements interfaces.Controller {
   constructor(private BlueprintUseCase: BlueprintUseCase) {}
 
-  @httpGet("/:namespace/:key")
-  private async getBlueprint(
-    @requestParam("namespace") namespace: BlueprintNamespaces,
-    @requestParam("key") key: AvailableBlueprints,
-    @request() request: IAuthenticatedRequest
-  ): Promise<any> {
-    return await this.BlueprintUseCase.getBluePrints(namespace, key);
-  }
-
   @httpGet("/:namespace")
   private async getBlueprintKey(@requestParam("namespace") namespace: BlueprintNamespaces): Promise<any> {
-    return await this.BlueprintUseCase.getBluePrintsKeys(namespace);
+    return await this.BlueprintUseCase.getBluePrintsValues(namespace);
   }
 }
--- src/useCases/ModuleSystem/blueprint/BlueprintUseCase.ts
+++ src/useCases/ModuleSystem/blueprint/BlueprintUseCase.ts
@@ -1,19 +1,12 @@
-import { AvailableBlueprints, BodiesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 import { BlueprintManager, BlueprintNamespaces } from "../../../providers/blueprint/BlueprintManager";
 import { provide } from "inversify-binding-decorators";
-import { Namespace } from "socket.io";
 
 @provide(BlueprintUseCase)
 export class BlueprintUseCase {
   constructor(private blueprintManager: BlueprintManager) {}
 
-  public async getBluePrints(namespace: BlueprintNamespaces, key: AvailableBlueprints) {
-    const blueprintData = await this.blueprintManager.getBlueprint(namespace, key);
-    return blueprintData;
-  }
-
-  public async getBluePrintsKeys(namespace: BlueprintNamespaces) {
-    const blueprintData = await this.blueprintManager.getAllBlueprintKeys(namespace);
+  public async getBluePrintsValues(namespace: BlueprintNamespaces) {
+    const blueprintData = await this.blueprintManager.getAllBlueprintValues(namespace);
     return blueprintData;
   }
 }
