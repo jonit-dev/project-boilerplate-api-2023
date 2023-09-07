6b342806f25b4f8dab2f792cf3ac4ec18d170838
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-09-07 10:30:15 -0700

    made crafting a little bit harder

--- src/providers/constants/CraftingConstants.ts
+++ src/providers/constants/CraftingConstants.ts
@@ -1,7 +1,7 @@
 export const CRAFTING_MIN_LEVEL_RATIO = 1.3;
 
 // remember this regulates mining, blacksmithing, fishing, etc.
-export const CRAFTING_DIFFICULTY_RATIO = 1.5; // higher means harder
+export const CRAFTING_DIFFICULTY_RATIO = 1.75; // higher means harder
 export const CRAFTING_BASE_CHANCE_IMPACT = 0.75; // higher means easier
 
-export const CRAFTING_ITEMS_CHANCE = 75; // base chance for crafting items with ingredients. higher means higher easier
+export const CRAFTING_ITEMS_CHANCE = 60; // base chance for crafting items with ingredients. higher means higher easier
