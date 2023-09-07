3ab34925786b7a2528b1cda8d385d08d1d58901b
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2023-09-06 21:55:09 -0300

    focus-swift-spell (#817)

---
+++ src/providers/spells/data/blueprints/berserker-warrior/index.ts
@@ -0,0 +1,6 @@
+import { SpellsBlueprint } from "@rpg-engine/shared";
+import { spellFocusSwitft } from "./SpellFocusSwift";
+
+export const berserkerWarriorSpellsIndex = {
+  [SpellsBlueprint.FocusSwift]: spellFocusSwitft,
+};
