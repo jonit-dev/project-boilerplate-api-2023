070d7fcc2aab8c90de98a7f44b382e5224ceae6e
guiazca <guiazca@gmail.com>
2023-09-11 23:19:31 +0100

    adding first commit (#832)

---
+++ src/providers/spells/data/index.ts
@@ -0,0 +1,6 @@
+import { IBlueprint } from "@providers/types/temp/BlueprintTypes";
+import { spellsBlueprints } from "./blueprints/index";
+
+export const spellsBlueprintsIndex: IBlueprint = {
+  ...spellsBlueprints,
+};
