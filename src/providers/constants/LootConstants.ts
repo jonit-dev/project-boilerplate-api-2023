e2ae1a5c7843d5095259c912851a86e80cb38801
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-09-29 14:08:58 -0700

    loot nerf + gold nerf

--- src/providers/constants/LootConstants.ts
+++ src/providers/constants/LootConstants.ts
@@ -1,10 +1,10 @@
 // General Loot
-export const NPC_LOOT_CHANCE_MULTIPLIER = 0.025;
+export const NPC_LOOT_CHANCE_MULTIPLIER = 0.01;
 
 // Gold
-export const LOOT_GOLD_QTY_RATIO = 0.6; // when it drops, ratio that regulates how much gold it would be
+export const LOOT_GOLD_QTY_RATIO = 0.4; // when it drops, ratio that regulates how much gold it would be
 export const LOOT_GOLD_DROP_CHANCE = 0.4; // chance that a mob drops gold. Static chance. Do not suffer influence of NPC_LOOT_CHANCE_MULTIPLIER
-export const LOOT_GOLD_MAX_HEALTH_WEIGHT = 1.25;
+export const LOOT_GOLD_MAX_HEALTH_WEIGHT = 1;
 export const LOOT_GOLD_STRENGTH_WEIGHT = 1.5;
 export const LOOT_GOLD_RESISTANCE_WEIGHT = 1;
 export const LOOT_GOLD_LEVEL_WEIGHT = 1.5;
