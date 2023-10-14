e64d1eca91a7e31382d4940b164dcbfdc3e81e95
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-10-14 13:38:57 -0700

    unique cron jobs

---
+++ src/providers/cronjobs/CronJobScheduler.ts
@@ -0,0 +1,27 @@
+import { Locker } from "@providers/locks/Locker";
+import { provide } from "inversify-binding-decorators";
+import nodeCron from "node-cron";
+
+@provide(CronJobScheduler)
+export class CronJobScheduler {
+  constructor(private locker: Locker) {}
+
+  public uniqueSchedule(id: string, cronString: string, task: Function): void {
+    const scheduledTask = nodeCron.schedule(cronString, async () => {
+      const canProceed = await this.locker.lock(`cron-job-unique-schedule-${id}`, 2000);
+
+      if (!canProceed) {
+        return;
+      }
+
+      try {
+        await task();
+      } catch (error) {
+        console.error(`An error occurred while running the task: ${error}`);
+      } finally {
+        scheduledTask.destroy(); // Destroy the cron job after execution.
+        await this.locker.unlock(`cron-job-unique-schedule-${id}`); // Unlock
+      }
+    });
+  }
+}
